646f2cf701ac4f52c5a18149b986b07f58456cd5
shueja-personal <j.a.shue@outlook.com>
2022-12-25 20:20:13 -0800

    missed some closing braces in the merge

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -88,8 +88,8 @@ class Sidebar extends Component<Props, State> {
         </Droppable>
         
       </DragDropContext>
-      <FontAwesomeIcon className={styles.Icon} icon={faGithub} }></FontAwesomeIcon>
-      <FontAwesomeIcon className={styles.Icon} icon={faDiscord} }></FontAwesomeIcon>
+      <FontAwesomeIcon className={styles.Icon} icon={faGithub}></FontAwesomeIcon>
+      <FontAwesomeIcon className={styles.Icon} icon={faDiscord}></FontAwesomeIcon>
       <FontAwesomeIcon className={styles.Icon} icon={faGear} onClick={()=>{this.context.uiState.setRobotConfigOpen(true)}}></FontAwesomeIcon>
       </div>
       <WaypointPanel waypoint={this.context.model.pathlist.activePath.lowestSelectedPoint()}></WaypointPanel>
