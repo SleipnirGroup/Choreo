a8bd40cffc8cf2419274a8dd28d6c230b51a6bcd
Joshua Nichols <45806130+JoshuaAN@users.noreply.github.com>
2022-12-27 00:25:12 -0500

    add interactivity

--- src/components/config/RobotConfigPanel.tsx
+++ src/components/config/RobotConfigPanel.tsx
@@ -1,12 +1,10 @@
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { observer } from 'mobx-react';
 import React, { Component } from 'react'
 import DocumentManagerContext from '../../document/DocumentManager';
 import NumberEntry from '../../util/NumberEntry';
 import CloseIcon from '@mui/icons-material/Close';
-import IconButton from "@mui/material/IconButton";
-const styles = require('./RobotConfigPanel.module.css').default;
+import IconButton from '@mui/material/IconButton';
+import styles from './RobotConfigPanel.module.css';
 
 type Props = {}
 
@@ -19,86 +17,86 @@ class RobotConfigPanel extends Component<Props, State> {
   render() {
     let config = this.context.model.robotConfig;
     return (
-    <div className={styles.Opacity}style={{display: (this.context.uiState.isRobotConfigOpen ? "block": "none")}}>
-      <div className={styles.RobotConfigPanel}>
+      <div className={styles.Opacity}style={{display: (this.context.uiState.isRobotConfigOpen ? "block": "none")}}>
+        <div className={styles.RobotConfigPanel}>
+          <h2>Robot Configuration</h2>
           <IconButton className={styles.ExitButton} onClick={()=>this.context.uiState.setRobotConfigOpen(false)}><CloseIcon /></IconButton>
-        <h2>Robot Configuration</h2>
+          <div className={styles.NumberEntryList}>
+            <NumberEntry 
+              title="Mass" 
+              suffix="kg" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.mass} 
+              setNumber={config!.setMass}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="Mass" 
-          suffix="kg" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.mass} 
-          setNumber={config!.setMass}
-          showCheckbox={false}/>
+            <NumberEntry 
+              title="MoI" 
+              suffix="kg · m²" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.moi} 
+              setNumber={config!.setMoI}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="MoI" 
-          suffix="kg · m²" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.moi} 
-          setNumber={config!.setMoI}
-          showCheckbox={false}/>
+            <NumberEntry 
+              title="Max Velocity" 
+              suffix="m/s" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.maxVelocity} 
+              setNumber={config!.setMaxVelocity}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="Max Velocity" 
-          suffix="m/s" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.maxVelocity} 
-          setNumber={config!.setMaxVelocity}
-          showCheckbox={false}/>
+            <NumberEntry 
+              title="Max Torque" 
+              suffix="N · m" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.maxTorque} 
+              setNumber={config!.setMaxTorque}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="Max Torque" 
-          suffix="N · m" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.maxTorque} 
-          setNumber={config!.setMaxTorque}
-          showCheckbox={false}/>
+            <NumberEntry 
+              title="Width" 
+              suffix="m" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.bumperWidth} 
+              setNumber={config!.setBumperWidth}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="Width" 
-          suffix="m" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.bumperWidth} 
-          setNumber={config!.setBumperWidth}
-          showCheckbox={false}/>
+            <NumberEntry 
+              title="Length" 
+              suffix="m" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.bumperLength} 
+              setNumber={config!.setBumperLength}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="Length" 
-          suffix="m" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.bumperLength} 
-          setNumber={config!.setBumperLength}
-          showCheckbox={false}/>
+            <NumberEntry 
+              title="Wheelbase" 
+              suffix="m" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.wheelbase} 
+              setNumber={config!.setWheelbase}
+              showCheckbox={false}/>
 
-        <NumberEntry 
-          title="Wheelbase" 
-          suffix="m" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.wheelbase} 
-          setNumber={config!.setWheelbase}
-          showCheckbox={false}/>
-
-        <NumberEntry 
-          title="Trackwidth" 
-          suffix="m" 
-          enabled={true} 
-          setEnabled={a=>null}
-          number={config.trackwidth} 
-          setNumber={config!.setTrackwidth}
-          showCheckbox={false}/>
-      </div>
+            <NumberEntry 
+              title="Trackwidth" 
+              suffix="m" 
+              enabled={true} 
+              setEnabled={a=>null}
+              number={config.trackwidth} 
+              setNumber={config!.setTrackwidth}
+              showCheckbox={false}/>
+          </div>
+        </div>
       </div>
     )
-
   }
 }
 export default observer(RobotConfigPanel);
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import Select, { OptionProps, components } from 'react-select';
 import DocumentManagerContext from '../../document/DocumentManager';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faEdit,
-  faGear,
-  faPlus,
-  faSave,
-  faTrash
-} from "@fortawesome/free-solid-svg-icons";
-import Button from "@mui/material/Button"
+import SettingsIcon from '@mui/icons-material/Settings';
+import SaveIcon from '@mui/icons-material/Save';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 import styles from './Navbar.module.css';
-import { IHolonomicPathStore } from '../../document/DocumentModel';
 import PathSelect from './PathSelect';
 
 type Props = {};
@@ -29,11 +22,9 @@ export default class Navbar extends Component<Props, State> {
       <div className={styles.Container}>
         <PathSelect></PathSelect>
         <span>
-        <Button id="addPath" className={styles.action} onClick={()=>this.context.uiState.setRobotConfigOpen(true)}>
-              <FontAwesomeIcon icon={faGear}></FontAwesomeIcon>
-        </Button>
-        <Button id="save" className={styles.action} onClick={()=>{this.context.model.saveFile()}}><FontAwesomeIcon icon={faSave}></FontAwesomeIcon></Button>
-        <Button id="generatePath" className={styles.action} onClick={()=>this.context.model.pathlist.activePath.generatePath()}>Generate Path</Button>
+          <IconButton className={styles.action} onClick={()=>this.context.uiState.setRobotConfigOpen(true)}><SettingsIcon /></IconButton>
+          <IconButton className={styles.action} onClick={()=>{this.context.model.saveFile()}}><SaveIcon /></IconButton>
+          <Button variant="contained" className={styles.action} onClick={()=>this.context.model.pathlist.activePath.generatePath()}>Generate Path</Button>
         </span>
       </div>
     )
