1c2e58973ca1d81775b041e8e6f61107cd428d14
shueja-personal <j.a.shue@outlook.com>
2022-12-27 23:50:14 -0800

    Heading box and handle hide when heading not constrained

--- src/components/field/svg/OverlayWaypoint.tsx
+++ src/components/field/svg/OverlayWaypoint.tsx
@@ -144,8 +144,9 @@ class OverlayWaypoint extends Component<Props, State> {
     return (
       <g ref={this.rootRef}>
         <g transform={`translate(${waypoint.x}, ${waypoint.y}) rotate(${waypoint.heading * 180 / Math.PI})`} id={this.appendIndexID("waypointGroup")}>
-            <this.BumperBox context={this.context} strokeColor={boxColorStr} strokeWidthPx={3}></this.BumperBox>
-           <circle cx={robotConfig.bumperLength / 2} cy={0} r={0.15 * Math.min(robotConfig.bumperLength, robotConfig.bumperWidth)} id={this.appendIndexID("rotateTarget")} fill={boxColorStr}></circle>
+          {this.props.waypoint.headingConstrained  && (<this.BumperBox context={this.context} strokeColor={boxColorStr} strokeWidthPx={3}></this.BumperBox>)}
+            
+            {this.props.waypoint.headingConstrained  && (<circle cx={robotConfig.bumperLength / 2} cy={0} r={0.15 * Math.min(robotConfig.bumperLength, robotConfig.bumperWidth)} id={this.appendIndexID("rotateTarget")} fill={boxColorStr}></circle>)}
            <circle cx={0} cy={0} r={0.15 * Math.min(robotConfig.bumperLength, robotConfig.bumperWidth)} id={this.appendIndexID("dragTarget")} fill={this.getDragTargetColor()}
            onClick={()=>this.selectWaypoint()} ></circle>
             
