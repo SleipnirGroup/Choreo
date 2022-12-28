1e07d93e6405359b3d0d8bd9fb9d0c5901815059
shueja-personal <j.a.shue@outlook.com>
2022-12-28 09:31:53 -0800

    Made path preview and connectors pointer-events none

--- src/components/field/svg/FieldOverlayRoot.tsx
+++ src/components/field/svg/FieldOverlayRoot.tsx
@@ -120,8 +120,8 @@ class FieldOverlayRoot extends Component<Props, State> {
             <line x1={0} y1={-DRAW_BOUND} x2={0} y2={DRAW_BOUND} stroke='darkgreen' strokeWidth={5 * GRID_STROKE} style={{pointerEvents:'none'}}></line>
             <line y1={0} x1={-DRAW_BOUND} y2={0} x2={DRAW_BOUND} stroke='darkred' strokeWidth={5 * GRID_STROKE} style={{pointerEvents:'none'}}></line>
             {/* Line paths */}
-            <polyline points={pathString} stroke="grey" strokeWidth={0.05} fill='transparent'></polyline>
-            <polyline points={generatedPathString} stroke="var(--select-yellow)" strokeWidth={0.05} fill='transparent'></polyline>
+            <polyline points={pathString} stroke="grey" strokeWidth={0.05} fill='transparent' style={{pointerEvents:'none'}}></polyline>
+            <polyline points={generatedPathString} stroke="var(--select-yellow)" strokeWidth={0.05} fill='transparent' style={{pointerEvents:'none'}}></polyline>
 
             {this.context.model.pathlist.activePath.waypoints.map((point, index)=>(
                 <OverlayWaypoint waypoint={point} index={index}></OverlayWaypoint>)
