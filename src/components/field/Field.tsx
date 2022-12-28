cb5134c0c22cc0c649c9e08de519fc87c2428897
shueja-personal <32416547+shueja-personal@users.noreply.github.com>
2022-12-28 13:46:21 -0800

    Clean up eslint (#34)

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -1,11 +1,8 @@
 
 import React, { Component} from 'react'
 
-import DocumentManagerContext from '../../document/DocumentManager';
-import FieldOverlay from './FieldOverlay';
 import styles from './Field.module.css';
 import FieldOverlayRoot from './svg/FieldOverlayRoot';
-import Moveable from 'react-moveable';
 
 type Props = {
 }
@@ -14,10 +11,7 @@ type State = {
 }
 
 export class Field extends Component<Props, State> {
-  constructor(props : Props) {
-    super(props);
-  }
- 
+
   render() {
     return (
 
