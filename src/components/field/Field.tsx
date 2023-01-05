3eea8191023463970c64b72ba365cd932c4d2849
shueja-personal <j.a.shue@outlook.com>
2023-01-04 23:41:57 -0800

    Increase size of generate button

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -30,7 +30,12 @@ export class Field extends Component<Props, State> {
         <FieldOverlayRoot ></FieldOverlayRoot>
         <WaypointPanel waypoint={this.context.model.pathlist.activePath.lowestSelectedPoint()}></WaypointPanel>
         
-        <IconButton color="primary" aria-label="add" size="medium" sx={{pointerEvents:'all', position:'absolute',bottom:10, right:10, borderRadius:'50%',boxShadow:'3px'}}
+        <IconButton color="primary" aria-label="add" size="large"
+          sx={{pointerEvents:'all', position:'absolute',
+            bottom:10, right:10,
+            transformOrigin:'100% 100%', transform:'scale(1.3)',
+            borderRadius:'50%',boxShadow:'3px',
+            }}
         onClick={()=>{this.context.model.pathlist.activePath.generatePath()}} disabled={!this.context.model.pathlist.activePath.canGenerate()}>
         <ShapeLineIcon></ShapeLineIcon>
         </IconButton>
