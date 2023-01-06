57264428a74859a9833b0b6de914453c3688256e
shueja-personal <j.a.shue@outlook.com>
2023-01-06 11:07:33 -0800

    revert formatter fail test and fix package.json

--- src/datatypes/HolonomicWaypoint.ts
+++ src/datatypes/HolonomicWaypoint.ts
@@ -4,7 +4,7 @@ export default class HolonomicWaypoint extends Waypoint {
   velocityX: number = 0;
   velocityY: number = 0;
   angularVelocity: number = 0;
-  velocityXConstrained: boolean= false;
+  velocityXConstrained: boolean = false;
   velocityYConstrained: boolean = false;
   velocityMagnitudeConstrained: boolean = false;
   angularVelocityConstrained: boolean = false;
