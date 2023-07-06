6e5301a455949350406140dd3ec5d36363710ef3
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-07-06 10:50:22 -0400

    Change from sidecar to resources

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -12,23 +12,21 @@ fn main() {
   const LIB_PATTERN: &str = "bin/*.dll";
   
   #[cfg(target_os = "macos")]
-  println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/");
+  println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/ -Wl,-rpath,@loader_path/../Resources/trajoptlib/");
   
   #[cfg(target_os = "linux")]
   println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/");
 
-  const NATIVE_LIB_OUT_DIR: &str = "target/release/bin";
+  const NATIVE_LIB_OUT_DIR: &str = "trajoptlib";
 
   let native_lib_search_dir = format!("target/release/build/trajoptlib-*/out/{}", LIB_PATTERN);
-
-  let target_triple = std::env::var("TARGET").unwrap();
 
   std::fs::create_dir_all(NATIVE_LIB_OUT_DIR).expect("Unable to create directory for native libraries");
 
   for entry in glob(&native_lib_search_dir).expect("Failed to find trajoptlib dylibs") {
     let file = entry.expect("error loading dylib");
-    let output_file = format!("{}/{}-{}",
-        NATIVE_LIB_OUT_DIR, file.file_name().unwrap().to_str().unwrap(), target_triple);
+    let output_file = format!("{}/{}",
+        NATIVE_LIB_OUT_DIR, file.file_name().unwrap().to_str().unwrap());
     fs::copy(file.clone(), output_file).expect("Error copying dylib");
   }
 
