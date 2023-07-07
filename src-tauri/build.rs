fc74faac764f5c171d2115b96f85e7b613184819
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-07-07 17:45:26 -0400

    More debug printing + linux glob fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -6,7 +6,7 @@ fn main() {
   const LIB_PATTERN: &str = "lib/lib*.dylib";
 
   #[cfg(target_os = "linux")]
-  const LIB_PATTERN: &str = "lib/lib*.so";
+  const LIB_PATTERN: &str = "lib/lib*.so*";
 
   #[cfg(target_os = "windows")]
   const LIB_PATTERN: &str = "bin/*.dll";
@@ -17,7 +17,7 @@ fn main() {
   #[cfg(target_os = "linux")]
   println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/");
 
-  const NATIVE_LIB_OUT_DIR: &str = "./";
+  const NATIVE_LIB_OUT_DIR: &str = ".";
 
   let native_lib_search_dir = format!("target/release/build/trajoptlib-*/out/{}", LIB_PATTERN);
 
@@ -27,6 +27,8 @@ fn main() {
     let file = entry.expect("error loading dylib");
     let output_file = format!("{}/{}",
         NATIVE_LIB_OUT_DIR, file.file_name().unwrap().to_str().unwrap());
+
+    println!("Copying file: {}", file.display());
     fs::copy(file.clone(), output_file).expect("Error copying dylib");
   }
 
