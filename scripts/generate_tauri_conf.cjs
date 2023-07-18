88f857cc16a2ab3a6d13b0e57dec55c524c88153
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-07-17 20:49:49 -0400

    New structure of node build scripts

---
+++ scripts/generate_tauri_conf.cjs
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { platform } = require('os');
+
+let dylibPattern = "*";
+
+switch (process.platform) {
+  case "win32":
+    dylibPattern = "*.dll";
+    break;
+  case "darwin":
+    dylibPattern = "lib*.dylib";
+  case "linux":
+    dylibPattern = "lib*.so";
+    break;
+}
+console.log("Using dylib pattern: \"" + dylibPattern + "\"");
+
+// The following step adds os-specific resources to the tauri bundle by
+// modifying the tauri.conf.json file. This step can be removed once
+// tauri allows this kind of resource pattern: "*.{dll,dylib,so}".
+
+let tauriConfJsonRaw = fs.readFileSync('src-tauri/tauri.conf.in.json');
+let tauriConfJson = JSON.parse(rawdata);
+
+jsonData.tauri.bundle.resources = [dylibPattern];
+
+const modifiedTauriConf = JSON.stringify(jsonData, null, 2) + "\n";
+
+// Write the modified tauri configuration
+fs.writeFileSync('src-tauri/tauri.conf.json', modifiedData);
+
