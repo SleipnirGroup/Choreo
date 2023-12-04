615503c3171294c1ff99516f818121acd0d29753
shueja-personal <j.a.shue@outlook.com>
2023-12-03 21:20:38 -0800

    format

--- src/document/PathListStore.ts
+++ src/document/PathListStore.ts
@@ -62,12 +62,8 @@ export const PathListStore = types
           name: usedName,
           waypoints: [],
         });
-        path
-          .addConstraint(ConstraintStores.StopPoint)
-          ?.setScope(["first"]);
-        path
-          .addConstraint(ConstraintStores.StopPoint)
-          ?.setScope(["last"]);
+        path.addConstraint(ConstraintStores.StopPoint)?.setScope(["first"]);
+        path.addConstraint(ConstraintStores.StopPoint)?.setScope(["last"]);
         self.paths.put(path);
         if (self.paths.size === 1 || select) {
           self.activePathUUID = newUUID;
