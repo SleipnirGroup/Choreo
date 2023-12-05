eb6f1c27dd79bde5ad0549ade8878a0947303ed2
Drew Williams <williams.r.drew@gmail.com>
2023-12-05 13:16:06 -0500

    trying to figure out lambda issues with swervecontroller

--- choreolib/src/main/native/cpp/choreo/lib/Choreo.cpp
+++ choreolib/src/main/native/cpp/choreo/lib/Choreo.cpp
@@ -2,6 +2,9 @@
 #include <frc/Filesystem.h>
 #include <wpi/MemoryBuffer.h>
 #include <wpi/json.h>
+#include <frc2/command/FunctionalCommand.h>
+#include <frc/Timer.h>
+#include <frc/DriverStation.h>
 
 using namespace choreolib;
 
@@ -22,4 +25,76 @@ ChoreoTrajectory Choreo::GetTrajectory(std::string trajName) {
 	ChoreoTrajectory traj;
 	choreolib::from_json(json, traj);
 	return traj;
+}
+
+frc2::CommandPtr Choreo::ChoreoSwerveCommand(const ChoreoTrajectory &trajectory,
+		std::function<frc::Pose2d()> poseSupplier,
+		frc::PIDController xController, frc::PIDController yController,
+		frc::PIDController rotationController,
+		std::function<void(frc::ChassisSpeeds)> outputChassisSpeeds,
+		bool useAllianceColor, frc2::Requirements requirements) {
+	return ChoreoSwerveCommand(trajectory, poseSupplier,
+			ChoreoSwerveController(xController, yController,
+					rotationController), outputChassisSpeeds, useAllianceColor,
+			requirements);
+}
+
+frc2::CommandPtr Choreo::ChoreoSwerveCommand(const ChoreoTrajectory &trajectory,
+		std::function<frc::Pose2d()> poseSupplier,
+		ChoreoControllerFunction controller,
+		std::function<void(frc::ChassisSpeeds)> outputChassisSpeeds,
+		bool useAllianceColor, frc2::Requirements requirements) {
+	frc::Timer timer;
+	return frc2::FunctionalCommand([timer] {
+		timer.Restart();
+	}
+			,
+			[timer, trajectory, poseSupplier, controller, useAllianceColor,
+					outputChassisSpeeds] {
+				bool mirror = false;
+				if (useAllianceColor) {
+					std::optional < frc::DriverStation::Alliance > alliance =
+							frc::DriverStation::GetAlliance();
+					mirror = alliance.has_value()
+							&& alliance.value()
+									== frc::DriverStation::Alliance::kRed;
+				}
+				outputChassisSpeeds(
+						controller(poseSupplier(),
+								trajectory.Sample(timer.Get(), mirror)));
+			},
+			[timer, outputChassisSpeeds](bool interrupted) {
+				timer.Stop();
+				if (interrupted) {
+					outputChassisSpeeds(frc::ChassisSpeeds { });
+				}
+			},
+			[timer, trajectory] {
+				return timer.HasElapsed(trajectory.GetTotalTime());
+			},
+			requirements).ToPtr();
+}
+
+ChoreoControllerFunction Choreo::ChoreoSwerveController(
+		frc::PIDController xController, frc::PIDController yController,
+		frc::PIDController rotationController) {
+	rotationController.EnableContinuousInput(-std::numbers::pi,
+			std::numbers::pi);
+	return [xController, yController, rotationController](
+			const frc::Pose2d &pose, ChoreoTrajectoryState &referenceState) {
+		units::meters_per_second_t xFF = referenceState.velocityX;
+		units::meters_per_second_t yFF = referenceState.velocityY;
+		units::radians_per_second_t rotationFF = referenceState.angularVelocity;
+
+		units::meters_per_second_t xFeedback { xController.Calculate(
+				pose.X().value(), referenceState.x.value()) };
+		units::meters_per_second_t yFeedback { yController.Calculate(
+				pose.Y().value(), referenceState.y.value()) };
+		units::radians_per_second_t rotationFeedback {
+				rotationController.Calculate(pose.Rotation().Radians().value(),
+						referenceState.heading.value()) };
+
+		return frc::ChassisSpeeds::FromFieldRelativeSpeeds(xFF + xFeedback,
+				yFF + yFeedback, rotationFF + rotationFeedback, pose.Rotation());
+	};
 }
--- choreolib/src/main/native/include/choreo/lib/Choreo.h
+++ choreolib/src/main/native/include/choreo/lib/Choreo.h
@@ -1,11 +1,34 @@
 #pragma once
 
 #include "ChoreoTrajectory.h"
+#include <frc2/command/CommandPtr.h>
+#include <frc/kinematics/ChassisSpeeds.h>
+#include <frc/controller/PIDController.h>
+#include "frc2/command/Requirements.h"
 
 namespace choreolib {
+
+using ChoreoControllerFunction = std::function<frc::ChassisSpeeds(frc::Pose2d, ChoreoTrajectoryState)>;
+
 class Choreo {
 public:
 	static ChoreoTrajectory GetTrajectory(std::string trajName);
+	static frc2::CommandPtr ChoreoSwerveCommand(
+			const ChoreoTrajectory &trajectory,
+			std::function<frc::Pose2d()> poseSupplier,
+			frc::PIDController xController, frc::PIDController yController,
+			frc::PIDController rotationController,
+			std::function<void(frc::ChassisSpeeds)> outputChassisSpeeds,
+			bool useAllianceColor, frc2::Requirements requirements = { });
+	static frc2::CommandPtr ChoreoSwerveCommand(
+			const ChoreoTrajectory &trajectory,
+			std::function<frc::Pose2d()> poseSupplier,
+			ChoreoControllerFunction controller,
+			std::function<void(frc::ChassisSpeeds)> outputChassisSpeeds,
+			bool useAllianceColor, frc2::Requirements requirements = { });
+	static ChoreoControllerFunction ChoreoSwerveController(
+			frc::PIDController xController, frc::PIDController yController,
+			frc::PIDController rotationController);
 
 private:
 };
