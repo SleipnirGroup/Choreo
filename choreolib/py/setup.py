7810c4b2417846c5aaf7e2c0fb063a1dea1906b2
Chris Gerth <chrisgerth010592@gmail.com>
2023-12-17 10:01:07 -0600

    Adding pypi publish

--- choreolib/py/setup.py
+++ choreolib/py/setup.py
@@ -7,15 +7,21 @@ gitDescribeResult = (
     .strip()
 )
 
-m = re.search(r"v[0-9]{4}\.[0-9]{1}.[0-9]{1}", gitDescribeResult)
+m = re.search(r"v([0-9]{4}\.[0-9]{1}.[0-9]{1})", gitDescribeResult)
 
 # Extract the first portion of the git describe result
 # which should be PEP440 compliant
 if m:
-    versionString = m.group(0)
+    versionString = m.group(1)
 else:
     print("Warning, no valid version found")
     versionString = gitDescribeResult
+
+# Put the version info into a python file for runtime access
+with open("choreolib/version.py", "w", encoding="utf-8") as fp:
+    fp.write(f'CHOREOLIB_VERSION="{versionString}"\n')
+    fp.write(f'CHOREOLIB_GIT_DESCRIBE="{gitDescribeResult}"\n')
+
 
 print(f"Building version {versionString}")
 
