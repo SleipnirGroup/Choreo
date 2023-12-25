f5e86da1e262b0136bf0809ab90cc31f738fa6dc
Max Niederman <max@maxniederman.com>
2023-12-24 16:44:06 -0800

    vendor UI fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "@fontsource/roboto";
+import "@fontsource/source-code-pro";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
