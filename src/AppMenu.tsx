6cab2cc33f62dbf8e8d1b9625699137b495acb5e
Awesomeplayer165 <dev@jacob2.dev>
2023-12-28 10:52:00 -0800

    format

--- src/AppMenu.tsx
+++ src/AppMenu.tsx
@@ -40,7 +40,9 @@ class AppMenu extends Component<Props, State> {
 
   private convertToRelative(filePath: string): string {
     return filePath.replace(
-      RegExp(`^(?:C:)?\\${path.sep}(Users|home)\\${path.sep}[a-zA-Z]+\\${path.sep}`),
+      RegExp(
+        `^(?:C:)?\\${path.sep}(Users|home)\\${path.sep}[a-zA-Z]+\\${path.sep}`
+      ),
       "~/"
     );
   }
