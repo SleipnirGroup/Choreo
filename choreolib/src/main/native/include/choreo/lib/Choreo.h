8861ee0455e04fff2dc75d53a4e22c7b84df52ee
shueja <32416547+shueja@users.noreply.github.com>
2024-01-16 13:26:21 -0800

    Update choreolib/src/main/native/include/choreo/lib/Choreo.h
    Co-authored-by: Tyler Veness <calcmogul@gmail.com>

--- choreolib/src/main/native/include/choreo/lib/Choreo.h
+++ choreolib/src/main/native/include/choreo/lib/Choreo.h
@@ -35,17 +35,19 @@ class Choreo {
   static ChoreoTrajectory GetTrajectory(std::string_view trajName);
 
   /**
-   * Loads the split parts of the specified trajectory. ChoreoLib expects split
-   * .traj files to be placed in
-   * src/main/deploy/choreo/[trajName].[segmentNumber].traj . <br/> This method
-   * determines the number of parts to load by counting the files that match the
-   * pattern "trajName.X.traj", where X is a string of digits. Let this count be
-   * N. It then attempts to load "trajName.1.traj" through "trajName.N.traj",
-   * consecutively counting up. If any of these files cannot be loaded, the
-   * method throws a runtime error.
+   * Loads the split parts of the specified trajectory.
+   *
+   * ChoreoLib expects split .traj files to be placed in
+   * src/main/deploy/choreo/[trajName].[segmentNumber].traj.
+   *
+   * This method determines the number of parts to load by counting the files
+   * that match the pattern "trajName.X.traj", where X is a string of digits.
+   * Let this count be N. It then attempts to load "trajName.1.traj" through
+   * "trajName.N.traj", consecutively counting up.
+   *
    * @param trajName the path name in Choreo. Do not include ".traj" here.
-   * @return The array of segments, in order. Will throw runtime error if any
-   * segment does not exist.
+   * @return The array of segments, in order.
+   * @throws std::runtime_error If any files cannot be loaded.
    */
   static ChoreoTrajectory[] GetTrajectoryGroup(
       std::string_view trajName);
