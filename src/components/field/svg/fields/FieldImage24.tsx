6ec84a179ece93ede9b155fef959c74443e770e8
shueja-personal <j.a.shue@outlook.com>
2024-01-22 14:34:08 -0800

    Use miklast high-quality field render, retrace field svg

--- src/components/field/svg/fields/FieldImage24.tsx
+++ src/components/field/svg/fields/FieldImage24.tsx
@@ -17,19 +17,19 @@ export default class FieldImage23 extends Component<Props, State> {
       <g id="layer1" transform={`scale(1, -1)`}>
         <g
           id="img"
-          transform={`translate(0, ${-WIDTH_M}) scale(${0.0175}) translate(-6, -10)`}
+          transform={`translate(0, ${-WIDTH_M}) scale(${0.01751}) translate(-6, -9.7)`}
           opacity={"100%"}
         >
           <rect
             x="477"
-            y="6.5"
+            y="8"
             width="3"
-            height="474"
+            height="472.5"
             fill="#ffffff"
             fillOpacity={1}
             stroke="#ffffff"
             strokeOpacity={1}
-            id="rect1"
+            id="midline"
           />
           <circle
             cx="478.5"
@@ -39,7 +39,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle1"
+            id="mid-top-note"
           />
           <circle
             cx="478.5"
@@ -49,7 +49,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle2"
+            id="mid-2-note"
           />
           <circle
             cx="478.5"
@@ -59,7 +59,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle3"
+            id="mid-3-note"
           />
           <circle
             cx="478.5"
@@ -69,7 +69,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle4"
+            id="mid-4-note"
           />
           <circle
             cx="478.5"
@@ -79,7 +79,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle5"
+            id="mid-bottom-note"
           />
           <circle
             cx="171"
@@ -89,7 +89,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle6"
+            id="blue-bottom-note"
           />
           <circle
             cx="171"
@@ -99,7 +99,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle7"
+            id="blue-top-note"
           />
           <circle
             cx="171"
@@ -109,113 +109,97 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle8"
-          />
-          <rect
-            x="338"
-            y="6"
-            width="4"
-            height="475"
-            fill="#0047A6"
-            id="rect8"
+            id="blue-mid-note"
           />
           <path
-            d="M310.167 198.589L310.167 288.078L232.667 243.334L310.167 198.589Z"
+            d="M310.167 198.589L310.167 288.778L232.667 243.934L310.167 198.589Z"
             fill="#555555"
-            id="path9"
+            id="blue-stage-structure"
           />
           <rect
-            x="348.356"
-            y="164.531"
-            width="32"
-            height="32"
-            transform="rotate(120 348.356 164.531)"
+            x="350.4"
+            y="162.531"
+            width="36"
+            height="36"
+            transform="rotate(120 350.4 162.531)"
             fill="#262626"
-            id="rect9"
+            id="blue-top-stage-base"
           />
           <rect
-            x="183"
-            y="228"
-            width="32"
-            height="32"
+            x="181"
+            y="226"
+            width="36"
+            height="36"
             fill="#262626"
-            id="rect10"
+            id="blue-left-stage-base"
           />
           <rect
-            x="320.644"
-            y="339.469"
-            width="32"
-            height="32"
-            transform="rotate(-120 320.644 339.469)"
+            x="320"
+            y="341.469"
+            width="36"
+            height="36"
+            transform="rotate(-120 320 341.469)"
             fill="#262626"
-            id="rect11"
+            id="blue-bottom-stage-base"
+          />
+          <rect
+            x="338.272"
+            y="8"
+            width="3"
+            height="473"
+            fill="#0047A6"
+            id="blue-wingline"
           />
           <path
-            d="M187.252 251.298L187.252 237.369L325.976 156.276L339.772 164.241L339.772 323.426L325.976 331.391L187.252 251.298Z"
+            d="M188.7 251.298L188.7 234.369L323.7 156.276L339.772 165.2L339.772 322.26L323.7 331.391L188.7 253.398Z"
             stroke="#0047A6"
-            strokeWidth="5"
+            strokeWidth="3"
             fill="none"
-            id="path8"
+            id="blue-stage-edge"
           />
           <path
             d="M337.062 166.094L324.938 159.094C323.981 158.541 322.758 158.869 322.206 159.826L278.804 235H192C190.895 235 190 235.895 190 237V251C190 252.105 190.895 253 192 253H278.804L322.206 328.174C322.758 329.131 323.981 329.459 324.938 328.906L337.062 321.906C338.019 321.354 338.347 320.131 337.794 319.174L294.392 244L337.794 168.826C338.347 167.869 338.019 166.646 337.062 166.094Z"
             fill="#555555"
-            id="path11"
+            id="blue-stage-tripod"
           />
           <path
-            d="M115 33V480"
+            d="M114.5 33V480"
             stroke="#ffffff"
             strokeOpacity={1}
-            strokeWidth="4"
+            strokeWidth="3"
             fill="none"
-            strokeLinejoin="round"
-            id="path12"
+            id="blue-auto-line"
           />
           <path
-            d="M7 33H193V8"
+            d="M5 33.4H192.8V8"
             stroke="#0047A6"
             strokeWidth="4"
             fill="none"
-            strokeLinejoin="round"
-            id="path13"
+            id="blue-amp-edge"
           />
-          <path d="M58 132L7 155V168L58 191V132Z" fill="#292929" id="path14" />
-          <path d="M7 168V221L58 191L7 168Z" fill="#D9D9D9" id="path15" />
-          <path d="M7 168V221L58 191L7 168Z" fill="#0047A6" id="path16" />
-          <path d="M7 102V155L58 132L7 102Z" fill="#D9D9D9" id="path17" />
-          <path d="M7 102V155L58 132L7 102Z" fill="#0047A6" id="path18" />
+          <rect
+            x="77"
+            y="8"
+            width="67"
+            height="2"
+            fill = "#0047A6"
+            stroke="none"
+            id="blue-amp-plate"
+          ></rect>
+          <path d="M57 132L6 155V168L58 191V132Z" fill="#292929" id="blue-sub-center" />
+          <path d="M6 168V221L58 191L6 168Z" fill="#0047A6" id="blue-sub-bottom" />
+          <path d="M6 102V155L58 132L6 102Z" fill="#0047A6" id="blue-sub-top" />
           <path
-            d="M7 127H31C32.1046 127 33 127.895 33 129V194C33 195.105 32.1046 196 31 196H7V127Z"
+            d="M6 127H31C32.1046 127 33 127.895 33 129V194C33 195.105 32.1046 196 31 196H6V127Z"
             fill="#585858"
-            id="path19"
-          />
-          <path
-            d="M141 6.5L141 8.5C141 9.60457 140.105 10.5 139 10.5L82 10.5C80.8954 10.5 80 9.60457 80 8.5L80 6.5L141 6.5Z"
-            fill="#585858"
-            id="path20"
-          />
-          <path
-            d="M10.9999 420.402L11.952 418.756C12.505 417.8 13.7292 417.472 14.6862 418.023L40.6788 433.003C41.6358 433.554 41.9633 434.777 41.4102 435.733L40.4581 437.379L10.9999 420.402Z"
-            fill="#585858"
-            id="path21"
-          />
-          <path
-            d="M42.1794 438.404L43.1315 436.758C43.6846 435.801 44.9087 435.473 45.8658 436.025L71.8583 451.004C72.8154 451.556 73.1428 452.778 72.5898 453.734L71.6377 455.38L42.1794 438.404Z"
-            fill="#585858"
-            id="path22"
-          />
-          <path
-            d="M73.359 456.405L74.3111 454.759C74.8641 453.803 76.0883 453.475 77.0453 454.026L103.038 469.006C103.995 469.558 104.322 470.78 103.769 471.736L102.817 473.382L73.359 456.405Z"
-            fill="#585858"
-            id="path23"
+            id="blue-speaker"
           />
           <path
             d="M6 387L111 448V477.5"
             stroke="#A1000F"
             strokeWidth="4"
-            strokeLinejoin="round"
             fill="none"
-            id="path24"
+            id="red-source-edge"
           />
           <circle
             cx="6"
@@ -226,7 +210,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle24"
+            id="red-bottom-note"
           />
           <circle
             cx="6"
@@ -237,7 +221,7 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle25"
+            id="red-top-note"
           />
           <circle
             cx="6"
@@ -248,119 +232,101 @@ export default class FieldImage23 extends Component<Props, State> {
             stroke="#d87000"
             strokeOpacity={1}
             strokeWidth={2.5}
-            id="circle26"
-          />
-          <rect
-            width="3.99996"
-            height="474"
-            transform="matrix(-1 0 0 1 619.003 6)"
-            fill="#A1000F"
-            id="rect26"
+            id="red-mid-note"
           />
           <path
-            d="M646.836 198.589L646.836 288.078L724.335 243.334L646.836 198.589Z"
+            d="M646.836 198.589L646.836 289.5L725.335 244L646.836 198.589Z"
             fill="#555555"
-            id="path27"
+            id="red-stage-structure"
           />
           <rect
-            width="32"
-            height="32"
-            transform="matrix(0.499997 0.866027 0.866023 -0.500004 608.647 164.531)"
+            width="36"
+            height="36"
+            transform="matrix(0.499997 0.866027 0.866023 -0.500004 606.647 162.531)"
             fill="#262626"
-            id="rect27"
+            id="red-top-stage-base"
           />
           <rect
-            width="32"
-            height="32"
-            transform="matrix(-1 0 0 1 774.002 228)"
+            width="36"
+            height="36"
+            transform="matrix(-1 0 0 1 776.002 226)"
             fill="#262626"
-            id="rect28"
+            id="red-right-stage-base"
           />
           <rect
-            width="32"
-            height="32"
-            transform="matrix(0.499996 -0.866027 -0.866023 -0.500003 636.359 339.469)"
+            width="36"
+            height="36"
+            transform="matrix(0.499996 -0.866027 -0.866023 -0.500003 638.359 342.469)"
             fill="#262626"
-            id="rect29"
+            id="red-bottom-stage-base"
+          />
+          <rect
+            width="3"
+            height="472.5"
+            transform="matrix(-1 0 0 1 618.732 8)"
+            fill="#A1000F"
+            id="red-wingline"
           />
           <path
-            d="M631.027 331.391L769.75 251.298L769.75 235.369L631.027 156.276L617.232 164.241L617.232 323.426L631.027 331.391Z"
+            d="M633.5 331.391L768.75 253.298L768.75 234.9L633.5 156.5L617.232 165.9L617.232 322.3L633.5 331.391Z"
             stroke="#A1000F"
-            strokeWidth="5"
+            strokeWidth="3"
             fill="none"
-            id="path26"
+            id="red-stage-edge"
           />
           <path
             d="M619.941 166.094L632.065 159.094C633.022 158.541 634.245 158.869 634.797 159.826L678.199 235H765.002C766.106 235 767.002 235.895 767.002 237V251C767.002 252.105 766.106 253 765.002 253H678.199L634.797 328.174C634.245 329.131 633.022 329.459 632.065 328.906L619.941 321.906C618.984 321.354 618.656 320.131 619.209 319.174L662.61 244L619.209 168.826C618.656 167.869 618.984 166.646 619.941 166.094Z"
             fill="#555555"
-            id="path29"
+            id="red-stage-tripod"
           />
           <path
-            d="M842.001 33V480"
+            d="M842.5 33V480"
             stroke="#ffffff"
             strokeOpacity={1}
-            strokeWidth="4"
-            strokeLinejoin="round"
+            strokeWidth="3"
             fill="none"
-            id="path30"
+            id="red-auto-line"
           />
           <path
-            d="M950 33H764.002V8"
+            d="M953 33.4H764.12V9"
             stroke="#A1000F"
-            strokeWidth="4"
-            strokeLinejoin="round"
+            strokeWidth="3"
             fill="none"
-            id="path31"
+            id="red-amp-edge"
           />
           <path
-            d="M899 132L950 155V168L899 191V132Z"
+            d="M899 132L951 155V168L899 191V132Z"
             fill="#292929"
-            id="path32"
+            id="red-sub-center"
           />
-          <path d="M950 168V221L899 191L950 168Z" fill="#D9D9D9" id="path33" />
-          <path d="M950 168V221L899 191L950 168Z" fill="#A1000F" id="path34" />
-          <path d="M950 102V155L899 132L950 102Z" fill="#D9D9D9" id="path35" />
-          <path d="M950 102V155L899 132L950 102Z" fill="#A1000F" id="path36" />
+          <path d="M951 168V221L899 191L951 168Z" fill="#A1000F" id="red-sub-bottom" />
+          <path d="M951 102V155L899 132L951 102Z" fill="#A1000F" id="red-sub-top" />
           <path
-            d="M950 127H926C924.896 127 924 127.895 924 129V194C924 195.105 924.896 196 926 196H950V127Z"
+            d="M951 127H926C924.896 127 924 127.895 924 129V194C924 195.105 924.896 196 926 196H951V127Z"
             fill="#585858"
-            id="path37"
+            id="red-speaker"
           />
+          <rect
+            x="812.5"
+            y="8"
+            width="67"
+            height="2"
+            fill="#A1000F"
+            id="red-amp-plate"
+          ></rect>
           <path
-            d="M816.001 6.5L816.001 8.5C816.001 9.60457 816.897 10.5 818.001 10.5L875.001 10.5C876.105 10.5 877.001 9.60457 877.001 8.5L877.001 6.5L816.001 6.5Z"
-            fill="#585858"
-            id="path38"
-          />
-          <path
-            d="M946 420.402L945.048 418.756C944.495 417.8 943.271 417.472 942.314 418.023L916.321 433.003C915.364 433.555 915.037 434.777 915.59 435.733L916.542 437.379L946 420.402Z"
-            fill="#585858"
-            id="path39"
-          />
-          <path
-            d="M914.821 438.404L913.868 436.758C913.315 435.802 912.091 435.474 911.134 436.025L885.142 451.005C884.185 451.556 883.857 452.778 884.41 453.735L885.362 455.381L914.821 438.404Z"
-            fill="#585858"
-            id="path40"
-          />
-          <path
-            d="M883.641 456.405L882.689 454.759C882.136 453.803 880.912 453.475 879.955 454.027L853.962 469.006C853.005 469.558 852.678 470.78 853.231 471.736L854.183 473.382L883.641 456.405Z"
-            fill="#585858"
-            id="path41"
-          />
-          <path
-            d="M951 387L846.001 448V479.5"
+            d="M951 387.75L845.7 448.75V479.5"
             stroke="#0047A6"
-            strokeWidth="4"
-            strokeLinejoin="round"
+            strokeWidth="3"
             fill="none"
-            id="path42"
+            id="blue-source-edge"
           />
           <path
-            d="M5 6V244V417.5L115 480H842L952 417.5V6H5Z"
+            d="M5 8.3V244V417.5L115 480H842L952 417.5V8.3H5Z"
             stroke="#FFFFFF"
             fill="none"
             strokeWidth="2"
-            strokeLinejoin="round"
-            id="path43"
+            id="border"
           />
         </g>
         {/* <rect 
