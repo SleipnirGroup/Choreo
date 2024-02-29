08509fcd3c4d96afec31f17824627ec69adeaedb
Tyler Veness <calcmogul@gmail.com>
2024-02-28 20:33:59 -0800

    Leave stdout destination console open
    Fixes #317, fixes #354.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,3 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use std::{fs, path::Path};
 use tauri::regex::{escape, Regex};
 use tauri::{
