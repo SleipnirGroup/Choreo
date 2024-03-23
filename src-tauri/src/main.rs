d6e0f6b8abcc4e9deea00c70e6549ebe9521236c
Tyler Veness <calcmogul@gmail.com>
2024-03-22 17:37:29 -0700

    Remove unused import (#406)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde_json::map;
 use std::{fs, path::Path};
 use tauri::regex::{escape, Regex};
 use tauri::{
