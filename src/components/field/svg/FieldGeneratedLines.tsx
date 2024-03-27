499e61c38abad6e0eb30f74b3eaec07f7d44ab77
Awesomeplayer165 <dev@jacob2.dev>
2024-03-27 00:19:33 -0700

    add selector, type abstraction, localstorage read comments!
    DOES NOT yet update based on updates in the menu immediately. need to switch to new path to invoke update"
    On branch path-gradient

--- src/components/field/svg/FieldGeneratedLines.tsx
+++ src/components/field/svg/FieldGeneratedLines.tsx
@@ -3,87 +3,22 @@ import DocumentManagerContext from "../../../document/DocumentManager";
 
 import { observer } from "mobx-react";
 import { SavedTrajectorySample } from "../../../document/DocumentSpecTypes";
+import { PathGradient, PathGradients } from "../../config/robotconfig/PathGradient";
+import LocalStorageKeys from "../../../util/LocalStorageKeys";
 
 type Props = object;
 
-type State = object;
+type State = {
+  selectedPathGradient: PathGradient;
+};
 
 class FieldPathLines extends Component<Props, State> {
   static contextType = DocumentManagerContext;
   declare context: React.ContextType<typeof DocumentManagerContext>;
-  state = {};
+  state = {
+    selectedPathGradient: PathGradients.Velocity
+  };
 
-  progress(point: SavedTrajectorySample, i:number, arr: SavedTrajectorySample[]) {
-    var t = 1 - (i / arr.length);
-    return `hsl(${100 * (t)}, 100%, 50%)`;
-  }
-
-  velocity(point: SavedTrajectorySample, i:number, arr: SavedTrajectorySample[]) {
-    var t = Math.hypot(point.velocityX, point.velocityY) / 5.0;
-    return `hsl(${100 * (t)}, 100%, 50%)`;
-  }
-
-  centripetal(point: SavedTrajectorySample, i:number, arr: SavedTrajectorySample[]) {
-    var t = 0;
-    if (i == 0 || i == arr.length-1) {
-      t = 0;
-    } else {
-      var A = arr[i-1];
-      var B = arr[i];
-      var C = arr[i+1];
-      var ab = Math.hypot(A.x-B.x, A.y-B.y);
-      var bc = Math.hypot(B.x-C.x, B.y-C.y);
-      var ca = Math.hypot(C.x-A.x, C.y-A.y);
-      // area using Heron's formula
-      var s = (ab + bc + ca) / 2;
-      var area = Math.sqrt(s * (s-ab) * (s-bc) * (s-ca));
-      var circumradius = ab * bc * ca / area / 4;
-
-      var vel = Math.hypot(point.velocityX, point.velocityY);
-
-      t = vel * vel / circumradius;
-      console.log (t, circumradius, area);
-      t /= 10;
-
-    }
-    //compute circumradius
-    return `hsl(${100 * (1-t)}, 100%, 50%)`;
-  }
-
-  accel(point: SavedTrajectorySample, i:number, arr: SavedTrajectorySample[]) {
-    var t = 0;
-    if (i == 0 || i == arr.length-1) {
-      t = 0;
-    } else {
-      var A = arr[i];
-      var B = arr[i+1];
-      var t = Math.hypot(B.velocityX-A.velocityX, B.velocityY-A.velocityY);
-      var dt = B.timestamp - A.timestamp;
-      console.log(t / dt);
-      t /= dt;
-      t /= 10;
-
-    }
-    //compute circumradius
-    return `hsl(${100 * (1-t)}, 100%, 50%)`;
-  }
-
-  dt(
-    point: SavedTrajectorySample, i:number, arr: SavedTrajectorySample[]) {
-      var t = 0;
-      if (i == 0 || i == arr.length-1) {
-        t = 0;
-      } else {
-        var A = arr[i];
-        var B = arr[i+1];
-        var dt = B.timestamp - A.timestamp;
-        t = 0.5 + 10 * (0.1-dt);
-        console.log(dt, t)
-  
-      }
-      //compute circumradius
-      return `hsl(${100 * (t)}, 100%, 50%)`;
-    }
   render() {
     let generatedPathString = "";
     let generated = this.context.model.document.pathlist.activePath.generated;
@@ -92,25 +27,30 @@ class FieldPathLines extends Component<Props, State> {
         generatedPathString += `${point.x},${point.y} `;
       }
     );
+
+    const pathGradientLocalStorage = localStorage.getItem(LocalStorageKeys.PATH_GRADIENT);
+    if (pathGradientLocalStorage) {
+      this.state.selectedPathGradient = PathGradients[pathGradientLocalStorage as keyof typeof PathGradients];
+    }
+
     return (
       <>
-
-        {/* <polyline
+        {this.state.selectedPathGradient == PathGradients.None && <polyline
           points={generatedPathString}
           stroke="var(--select-yellow)"
           strokeWidth={0.05}
           fill="transparent"
           style={{ pointerEvents: "none" }}
-        ></polyline> */}
+        ></polyline>}
         <g>
           {generated.map(
-            (point, i, arr)=>{
-              if (i == arr.length - 1){ return (<></>)}
-              var point2 = arr[i+1];
+            (point, i, arr) => {
+              if (i == arr.length - 1) { return (<></>) }
+              var point2 = arr[i + 1];
               // 0 t = red, 1 t = green
               return (
                 <line x1={point.x} y1={point.y} x2={point2.x} y2={point2.y}
-                strokeWidth={0.05} stroke={this.dt(point, i, arr)}></line>
+                  strokeWidth={0.05} stroke={this.state.selectedPathGradient.function(point, i, arr)}></line>
               )
             })}
         </g>
