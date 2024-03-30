eab155f8de92068654ffec2ce517913ac1c917ca
Awesomeplayer165 <dev@jacob2.dev>
2024-03-29 21:39:39 -0700

    remove path gradient from beta config

--- src/components/config/BetasConfigPanel.tsx
+++ src/components/config/BetasConfigPanel.tsx
@@ -71,37 +71,6 @@ class BetasConfigPanel extends Component<Props, State> {
             ></Switch>
           </Tooltip>
         </div>
-        <div
-          style={{
-            marginLeft: `${1 * this.rowGap}px`,
-            marginRight: `${1 * this.rowGap}px`,
-            marginTop: `${1 * this.rowGap}px`,
-            marginBottom: `${1 * this.rowGap}px`
-          }}
-        >
-          <span
-            className={inputStyles.Title}
-            style={{ verticalAlign: "middle", marginRight: "10px" }}
-          >
-            Path Gradient
-          </span>
-          <Select
-            defaultValue={PathGradients.Velocity.name}
-            value={this.context.model.uiState.selectedPathGradient}
-            onChange={(event) => {
-              const key = event.target.value as keyof typeof PathGradients;
-              this.context.model.uiState.setSelectedPathGradient(
-                PathGradients[key]
-              );
-            }}
-          >
-            {Object.keys(PathGradients).map((key) => (
-              <MenuItem value={key}>
-                {PathGradients[key as keyof typeof PathGradients].name}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
       </div>
     );
   }
