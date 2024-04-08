a050ee081f564b40ca3e32d47243781fee4a2163
shueja-personal <j.a.shue@outlook.com>
2024-04-08 14:32:06 -0700

    Turn off console output

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use serde::{Deserialize, Serialize};
 use std::mem::MaybeUninit;
