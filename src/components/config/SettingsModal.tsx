f944fa5841a25cc2a91da032a286868f732bb0b2
Awesomeplayer165 <dev@jacob2.dev>
2024-04-09 23:03:40 -0700

    add fade in/out effect

--- src/components/config/SettingsModal.tsx
+++ src/components/config/SettingsModal.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react";
 import React, { Component } from "react";
 import DocumentManagerContext from "../../document/DocumentManager";
-import { IconButton, Modal, Tab, Tabs } from "@mui/material";
+import { Fade, IconButton, Modal, Tab, Tabs } from "@mui/material";
 import RobotConfigPanel from "./robotconfig/RobotConfigPanel";
 import { Close } from "@mui/icons-material";
 import ExportConfigPanel from "./ExportConfigPanel";
@@ -21,7 +21,9 @@ class SettingsModal extends Component<Props, State> {
       <Modal
         open={uiState.robotConfigOpen}
         onClose={() => uiState.setRobotConfigOpen(false)}
+        closeAfterTransition
       >
+      <Fade in={uiState.robotConfigOpen}>
         <div
           style={{
             position: "absolute",
@@ -39,48 +41,53 @@ class SettingsModal extends Component<Props, State> {
             flexDirection: "column"
           }}
         >
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between"
-            }}
-          >
-            <Tabs
-              value={uiState.settingsTab}
-              onChange={(e, newValue) => uiState.setSettingsTab(newValue)}
-              centered
-              textColor="inherit"
-              sx={{
-                ".MuiTabs-indicator": {
-                  "background-color": "var(--select-yellow)"
-                },
-                ".Mui-selected": {
-                  color: "white"
-                },
-                justifySelf: "center",
-                marginTop: "8px",
-                marginRight: "8px",
-                marginLeft: "8px"
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between"
               }}
             >
-              <Tab label="Robot Config" />
-              <Tab label="Export Config" />
-              <Tab label="Betas" />
-            </Tabs>
-            <IconButton onClick={() => uiState.setRobotConfigOpen(false)}>
-              <Close></Close>
-            </IconButton>
+              <Tabs
+                value={uiState.settingsTab}
+                onChange={(e, newValue) => uiState.setSettingsTab(newValue)}
+                centered
+                textColor="inherit"
+                sx={{
+                  ".MuiTabs-indicator": {
+                    "background-color": "var(--select-yellow)"
+                  },
+                  ".Mui-selected": {
+                    color: "white"
+                  },
+                  justifySelf: "center",
+                  marginTop: "8px",
+                  marginRight: "8px",
+                  marginLeft: "8px"
+                }}
+              >
+                <Tab label="Robot Config" />
+                <Tab label="Export Config" />
+                <Tab label="Betas" />
+              </Tabs>
+              <IconButton onClick={() => uiState.setRobotConfigOpen(false)}>
+                <Close></Close>
+              </IconButton>
+            </div>
+            <div style={{ paddingTop: 8, flexGrow: 1, overflowY: "scroll" }}>
+              {uiState.settingsTab == 0 && (
+                <RobotConfigPanel></RobotConfigPanel>
+              )}
+              {uiState.settingsTab == 1 && (
+                <ExportConfigPanel></ExportConfigPanel>
+              )}
+              {uiState.settingsTab == 2 && (
+                <BetasConfigPanel></BetasConfigPanel>
+              )}
+            </div>
           </div>
-          <div style={{ paddingTop: 8, flexGrow: 1, overflowY: "scroll" }}>
-            {uiState.settingsTab == 0 && <RobotConfigPanel></RobotConfigPanel>}
-            {uiState.settingsTab == 1 && (
-              <ExportConfigPanel></ExportConfigPanel>
-            )}
-            {uiState.settingsTab == 2 && <BetasConfigPanel></BetasConfigPanel>}
-          </div>
-        </div>
+        </Fade>
       </Modal>
     );
   }
