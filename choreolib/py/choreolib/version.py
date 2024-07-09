e25a83f53988f84c1c8ad8aec9afa08dd02e2350
Tyler Veness <calcmogul@gmail.com>
2024-07-08 23:18:36 -0700

    Replace setup.py with pyproject.toml

--- choreolib/py/choreolib/version.py
+++ choreolib/py/choreolib/version.py
@@ -1,2 +1,20 @@
-CHOREOLIB_VERSION = "2024.2.3"
-CHOREOLIB_GIT_DESCRIBE = "v2024.2.3-24-gafb28f6"
+import re
+import subprocess
+
+
+def get_version():
+    proc = subprocess.run(
+        [
+            "git",
+            "describe",
+            "--tags",
+        ],
+        check=True,
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+    )
+    # If there is no main branch, default to 0.0.0
+    if proc.returncode:
+        return "0.0.0"
+    else:
+        return re.search(r"v([0-9]+\.[0-9]+\.[0-9]+)", proc.stdout).group(1)
