ef3e111edb6a14ec4915ccfff21cda4a80600795
Tyler Veness <calcmogul@gmail.com>
2024-07-14 11:36:05 -0700

    Clean up swerve drivetrain comments

--- trajoptlib/include/trajopt/drivetrain/SwerveDrivetrain.hpp
+++ trajoptlib/include/trajopt/drivetrain/SwerveDrivetrain.hpp
@@ -10,10 +10,7 @@
 namespace trajopt {
 
 /**
- * @brief This class represents a swerve drivetrain robot. It includes the
- * physical properties necessary to accurately model the dynamics of the system.
- * An arbitrary number of swerve modules can be specified, but typically it will
- * be four. The order the swerve modules are listed does not matter.
+ * A swerve drivetrain physical model.
  */
 struct TRAJOPT_DLLEXPORT SwerveDrivetrain {
   /// The mass of the robot (kg).
@@ -22,17 +19,17 @@ struct TRAJOPT_DLLEXPORT SwerveDrivetrain {
   /// The moment of inertia of the robot about the origin (kg−m²).
   double moi;
 
-  /// Radius of wheel (meters).
+  /// Radius of wheel (m).
   double wheelRadius;
 
   /// Maximum angular velocity of wheel (rad/s).
   double wheelMaxAngularVelocity;
 
-  /// Maximum torque (N−m) applied to wheel.
+  /// Maximum torque applied to wheel (N−m).
   double wheelMaxTorque;
 
-  /// Translation (meters) of each swerve module from the origin of the robot
-  /// coordinate system to the center of the module. There's usually one in each
+  /// Translation of each swerve module from the origin of the robot coordinate
+  /// system to the center of the module (m). There's usually one in each
   /// corner.
   std::vector<Translation2d> modules;
 };
