e3ce7fd87597098381085fb2bd6fe218d9251562
Tyler Veness <calcmogul@gmail.com>
2024-07-14 00:14:23 -0700

    Clean up Rust swerve example

--- trajoptlib/examples/swerve.rs
+++ trajoptlib/examples/swerve.rs
@@ -37,15 +37,19 @@ fn main() {
     };
 
     let mut path = SwervePathBuilder::new();
+
     path.add_progress_callback(|traj, handle| println!("{:?}: handle {}", traj, handle));
     path.set_drivetrain(&drivetrain);
     path.set_bumpers(1.3, 1.3);
+
     path.pose_wpt(0, 0.0, 0.0, 0.0);
     path.pose_wpt(1, 1.0, 0.0, 0.0);
+
     path.wpt_angular_velocity_max_magnitude(0, 0.0);
     path.wpt_angular_velocity_max_magnitude(1, 0.0);
     path.sgmt_circle_obstacle(0, 1, 0.5, 0.1, 0.2);
+
     path.set_control_interval_counts(vec![40]);
-    println!("setup complete");
+
     println!("{:?}", path.generate(true, 0));
 }
