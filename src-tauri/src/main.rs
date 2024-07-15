5cd452aae409abdcc7decb7cd03fbc6778c8de68
Tyler Veness <calcmogul@gmail.com>
2024-07-14 20:12:37 -0700

    Fix Choreo build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,7 +11,7 @@ use tauri::{
     api::{dialog::blocking::FileDialogBuilder, file},
     Manager,
 };
-use trajoptlib::{HolonomicTrajectory, Pose2d, SwerveDrivetrain, SwervePathBuilder, Translation2d};
+use trajoptlib::{Pose2d, SwerveDrivetrain, SwervePathBuilder, SwerveTrajectory, Translation2d};
 
 #[derive(Clone, serde::Serialize, Debug)]
 struct OpenFileEventPayload<'a> {
@@ -271,7 +271,7 @@ async fn cancel() {
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
 struct ProgressUpdate {
-    traj: HolonomicTrajectory,
+    traj: SwerveTrajectory,
     handle: i64,
 }
 
@@ -286,7 +286,7 @@ async fn generate_trajectory(
     polygonObstacles: Vec<PolygonObstacle>,
     // The handle referring to this path for the solver state callback
     handle: i64,
-) -> Result<HolonomicTrajectory, String> {
+) -> Result<SwerveTrajectory, String> {
     let mut path_builder = SwervePathBuilder::new();
     let mut wpt_cnt: usize = 0;
     let mut rm: Vec<usize> = Vec::new();
@@ -454,7 +454,7 @@ async fn generate_trajectory(
  * constructed in a static context.
  */
 static PROGRESS_SENDER_LOCK: OnceLock<Sender<ProgressUpdate>> = OnceLock::new();
-fn solver_status_callback(traj: HolonomicTrajectory, handle: i64) {
+fn solver_status_callback(traj: SwerveTrajectory, handle: i64) {
     let tx_opt = PROGRESS_SENDER_LOCK.get();
     if let Some(tx) = tx_opt {
         let _ = tx.send(ProgressUpdate { traj, handle });
