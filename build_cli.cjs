f5fe4bf59d9dd80673adbe55a52cea4c9331775d
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-09-01 02:42:37 -0400

    changed copy to move in build_cli.cjs

--- build_cli.cjs
+++ build_cli.cjs
@@ -1,40 +1,42 @@
-const { exec, execSync } = require('child_process');
-const fs = require('fs');
+const { exec, execSync } = require("child_process");
+const fs = require("fs");
 
 function get_executable_extension() {
-    if (process.platform === "win32") {
-        return ".exe";
-    }
-    return "";
+  if (process.platform === "win32") {
+    return ".exe";
+  }
+  return "";
 }
 
 console.log("Building CLI...");
 
 // build cli
 const build = exec("cargo build --release -p choreo-cli");
-build.stdout.on('data', data => print(data, end=''));
-build.stderr.on('data', data => console.error(data));
+build.stdout.on("data", (data) => print(data, (end = "")));
+build.stderr.on("data", (data) => console.error(data));
 
 // get target triple
 let targetTriple = "";
 const output = execSync("rustc -vV").toString();
 for (const line of output.split("\n")) {
-    if (line.includes("host")) {
-        targetTriple = line.split(" ")[1];
-    }
+  if (line.includes("host")) {
+    targetTriple = line.split(" ")[1];
+  }
 }
 if (targetTriple === "") {
-    throw new Error("Could not find target triple: " + output);
+  throw new Error("Could not find target triple: " + output);
 }
 
 // move and rename cli
 const executableExtension = get_executable_extension();
-build.once('exit', code => {
-    if (code !== 0) {
-        throw new Error("Build failed");
-    }
-    if (!fs.existsSync("cli")) {
-        fs.mkdirSync("cli");
-    }
-    execSync(`cp target/release/choreo-cli${executableExtension} cli/choreo-cli-${targetTriple}${executableExtension}`);
+build.once("exit", (code) => {
+  if (code !== 0) {
+    throw new Error("Build failed");
+  }
+  if (!fs.existsSync("cli")) {
+    fs.mkdirSync("cli");
+  }
+  execSync(
+    `mv target/release/choreo-cli${executableExtension} cli/choreo-cli-${targetTriple}${executableExtension}`
+  );
 });
