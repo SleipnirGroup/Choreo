3ea6c76b4a65a88a1e428897e2be4c7dbb1d691e
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-09-01 02:04:40 -0400

    added cli to build process and bundle

---
+++ build_cli.cjs
@@ -0,0 +1,41 @@
+const { exec, execSync } = require('child_process');
+const fs = require('fs');
+
+function get_executable_extension() {
+    if (process.platform === "win32") {
+        return ".exe";
+    }
+    return "";
+}
+
+console.log("Building CLI...");
+
+// build cli
+const build = exec("cargo build --release -p choreo-cli");
+build.stdout.on('data', data => print(data, end=''));
+build.stderr.on('data', data => console.error(data));
+
+// get target triple
+let targetTriple = "";
+const output = execSync("rustc -vV").toString();
+for (const line of output.split("\n")) {
+    if (line.includes("host")) {
+        targetTriple = line.split(" ")[1];
+    }
+}
+if (targetTriple === "") {
+    throw new Error("Could not find target triple: " + output);
+}
+
+// move and rename cli
+const executableExtension = get_executable_extension();
+build.once('exit', code => {
+    if (code !== 0) {
+        throw new Error("Build failed");
+    }
+    const f = fs.readFileSync(`target/release/choreo-cli${executableExtension}`);
+    if (!fs.existsSync("cli")) {
+        fs.mkdirSync("cli");
+    }
+    fs.writeFileSync(`cli/choreo-cli-${targetTriple}${executableExtension}`, f);
+});
