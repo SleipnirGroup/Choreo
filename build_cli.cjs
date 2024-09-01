ab59af43eacbb186831d9f19d3ae1c0b1adf80fe
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-09-01 14:06:04 -0400

    hopefully fixed CI

--- build_cli.cjs
+++ build_cli.cjs
@@ -1,43 +1,37 @@
 const { exec, execSync } = require("child_process");
 const fs = require("fs");
 
-function get_executable_extension() {
-  if (process.env.TAURI_PLATFORM === "windows") {
-    return ".exe";
-  }
-  return "";
-}
-
 console.log("Building CLI...");
 
-console.log(process.env.TAURI_PLATFORM);
-console.log(process.env.TAURI_ARCH);
-console.log(process.env.TAURI_FAMILY);
-console.log(process.env.TAURI_PLATFORM_VERSION);
-console.log(process.env.TAURI_PLATFORM_TYPE);
-console.log(process.env.TAURI_DEBUG);
+// console.log(process.env.TAURI_PLATFORM);
+// console.log(process.env.TAURI_ARCH);
+// console.log(process.env.TAURI_FAMILY);
+// console.log(process.env.TAURI_PLATFORM_VERSION);
+// console.log(process.env.TAURI_PLATFORM_TYPE);
+// console.log(process.env.TAURI_DEBUG);
 
-// build cli
-const build = exec("cargo build --release -p choreo-cli");
-build.stdout.on("data", (data) => print(data, (end = "")));
-build.stderr.on("data", (data) => console.error(data));
-
-// get target triple
 let targetTriple = "";
-const output = execSync("rustc -vV").toString();
-for (const line of output.split("\n")) {
-  if (line.includes("host")) {
-    targetTriple = line.split(" ")[1];
-  }
-}
-if (targetTriple === "") {
-  throw new Error("Could not find target triple: " + output);
+let extension = "";
+const platType = process.env.TAURI_PLATFORM_TYPE;
+const platArch = process.env.TAURI_ARCH;
+if (platType === "Darwin") {
+  targetTriple = `${platArch}-apple-darwin`;
+} else if (platType === "Linux") {
+  targetTriple = `${platArch}-unknown-linux-gnu`;
+} else if (platType === "Windows_NT") {
+  targetTriple = `${platArch}-pc-windows-msvc`;
+  extension = ".exe";
 }
 
 console.log("Target triple: " + targetTriple);
 
+// build cli
+const build = exec(`cargo build --release -p choreo-cli --target ${targetTriple}`);
+build.stdout.on("data", (data) => print(data, (end = "")));
+build.stderr.on("data", (data) => console.error(data));
+
+
 // move and rename cli
-const executableExtension = get_executable_extension();
 build.once("exit", (code) => {
   if (code !== 0) {
     throw new Error("Build failed");
@@ -46,6 +40,6 @@ build.once("exit", (code) => {
     fs.mkdirSync("cli");
   }
   execSync(
-    `mv target/release/choreo-cli${executableExtension} cli/choreo-cli-${targetTriple}${executableExtension}`
+    `mv target/release/choreo-cli${extension} cli/choreo-cli-${targetTriple}${extension}`
   );
 });
