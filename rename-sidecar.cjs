00e8a50ef9080a7b09b9a9f142f00d2341187e5e
Tyler Veness <calcmogul@gmail.com>
2024-09-08 13:20:41 -0700

    [choreo] Clean up rename-sidecar.cjs

--- rename-sidecar.cjs
+++ rename-sidecar.cjs
@@ -3,38 +3,27 @@ const { exit } = require("process");
 
 const [name, newPathRoot] = process.argv.slice(2);
 
-let targetTriple = "";
-let extension = "";
+const extension = process.env.TAURI_FAMILY === "windows" ? ".exe" : "";
+const targetTriple = process.env.TAURI_TARGET_TRIPLE;
 
-const platType = process.env.TAURI_PLATFORM_TYPE;
-const platArch = process.env.TAURI_ARCH;
-if (platType === "Darwin") {
-  targetTriple = `${platArch}-apple-darwin`;
-} else if (platType === "Linux") {
-  targetTriple = `${platArch}-unknown-linux-gnu`;
-} else if (platType === "Windows_NT") {
-  targetTriple = `${platArch}-pc-windows-msvc`;
-  extension = ".exe";
-}
-
-const newPath = `${newPathRoot}/${name}-${targetTriple}${extension}`;
 const oldPathNative = `./target/release/${name}${extension}`;
 const oldPath = `./target/${targetTriple}/release/${name}${extension}`;
+const newPath = `${newPathRoot}/${name}-${targetTriple}${extension}`;
 
-const oldPathNativeExists = fs.existsSync(`${oldPathNative}`);
-const oldPathExists = fs.existsSync(`${oldPath}`);
-
-if (!oldPathNativeExists && !oldPathExists) {
+let path;
+if (fs.existsSync(oldPathNative)) {
+  path = oldPathNative;
+} else if (fs.existsSync(oldPath)) {
+  path = oldPath;
+} else {
   console.log(`No file found at ${oldPathNative} or ${oldPath}`);
   exit(1);
 }
 
-const path = oldPathNativeExists ? oldPathNative : oldPath;
-
 fs.rename(path, newPath, (err) => {
   if (err) {
-    console.error(`Error renaming ${oldPath} to ${newPath}: ${err}`);
+    console.error(`Error renaming ${path} to ${newPath}: ${err}`);
     exit(1);
   }
-  console.log(`Renamed ${oldPath} to ${newPath}`);
+  console.log(`Renamed ${path} to ${newPath}`);
 });
