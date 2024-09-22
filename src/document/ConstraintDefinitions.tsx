bf4cea68c89b11b80915cd77c0e04721ae9f6ca7
shueja-personal <j.a.shue@outlook.com>
2024-09-22 15:43:30 -0700

    Remove ts-broken ObjectTyped.keys

--- src/document/ConstraintDefinitions.tsx
+++ src/document/ConstraintDefinitions.tsx
@@ -6,7 +6,6 @@ import {
   SystemUpdateAlt
 } from "@mui/icons-material";
 import { JSXElementConstructor, ReactElement } from "react";
-import { ObjectTyped } from "../util/ObjectTyped";
 import { Expr } from "./2025/DocumentTypes";
 import { Dimension, DimensionName, Dimensions } from "./ExpressionStore";
 
@@ -243,4 +242,4 @@ export const ConstraintDefinitions: defs = {
 };
 
 export type ConstraintKey = keyof DataMap;
-export const consts = ObjectTyped.values(ConstraintDefinitions);
+export const consts = Object.values(ConstraintDefinitions);
