d13e05ac5429965fcbf30c251c0374d8f85f54f5
Drew Williams <williams.r.drew@gmail.com>
2024-09-24 14:27:46 -0400

    updated comments for clarity

--- choreolib/src/main/native/include/choreo/trajectory/TrajectorySample.h
+++ choreolib/src/main/native/include/choreo/trajectory/TrajectorySample.h
@@ -30,7 +30,7 @@ concept TrajectorySample = requires(T t, units::second_t time, T tother, int yea
   { t.GetChassisSpeeds() } -> std::same_as<frc::ChassisSpeeds>;
   { t.OffsetBy(time) } -> std::same_as<T>;
   { t.Interpolate(tother, time) } -> std::same_as<T>;
-  //This is to get around gcc bug on roborio, so currently it can't be fully generic
+  // FIXME: This works around a roboRIO GCC internal compiler error; it can't be fully generic
   { t.template Flipped<2022>() } -> std::same_as<T>;
   { t.template Flipped<2023>() } -> std::same_as<T>;
   { t.template Flipped<2024>() } -> std::same_as<T>;
