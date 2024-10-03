3a2a9d632e724e034724fc602e0e82a650363dd9
bruingineer <joshua.g.camarena@gmail.com>
2024-10-03 02:13:54 -0700

    make example use realistic numbers for drivetrain

--- trajoptlib/examples/differential.rs
+++ trajoptlib/examples/differential.rs
@@ -1,13 +1,15 @@
+use core::f64;
+
 use trajoptlib::{DifferentialDrivetrain, DifferentialPathBuilder, PathBuilder};
 
 fn main() {
     let drivetrain = DifferentialDrivetrain {
-        mass: 45.0,
-        moi: 6.0,
-        wheel_radius: 0.08,
-        wheel_max_angular_velocity: 70.0,
-        wheel_max_torque: 5.0,
-        trackwidth: 6.0,
+        mass: 45.0, // kg
+        moi: 6.0, // kg m^2
+        wheel_radius: 0.05, // m
+        wheel_max_angular_velocity: 6000.0 * 2.0 * f64::consts::PI / (60.0 * 6.5), // motor rpm / 60 * 2pi / gear ratio
+        wheel_max_torque: 0.9, // N m
+        trackwidth: 0.5588, // m
     };
 
     let mut path = DifferentialPathBuilder::new();
@@ -23,7 +25,7 @@ fn main() {
 
     path.wpt_angular_velocity_max_magnitude(0, 0.0);
     path.wpt_angular_velocity_max_magnitude(1, 0.0);
-    path.sgmt_keep_out_circle(0, 1, 0.5, 0.1, 0.2);
+    // path.sgmt_keep_out_circle(0, 1, 0.5, 0.1, 0.2);
 
     path.set_control_interval_counts(vec![40]);
 
