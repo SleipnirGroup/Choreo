6670eed702303ae9938944d28fce778342ea3d0a
Tyler Veness <calcmogul@gmail.com>
2024-10-04 11:37:30 -0700

    [trajoptlib] Sort constraint types lexicographically

--- trajoptlib/include/trajopt/constraint/Constraint.hpp
+++ trajoptlib/include/trajopt/constraint/Constraint.hpp
@@ -3,7 +3,6 @@
 #pragma once
 
 #include <concepts>
-#include <type_traits>
 #include <variant>
 
 #include <sleipnir/autodiff/Variable.hpp>
@@ -56,19 +55,27 @@ static_assert(ConstraintType<LinearVelocityDirectionConstraint>);
 static_assert(ConstraintType<LinearVelocityMaxMagnitudeConstraint>);
 static_assert(ConstraintType<PointAtConstraint>);
 static_assert(ConstraintType<PointLineConstraint>);
-static_assert(ConstraintType<PointPointMinConstraint>);
+static_assert(ConstraintType<PointLineRegionConstraint>);
 static_assert(ConstraintType<PointPointMaxConstraint>);
+static_assert(ConstraintType<PointPointMinConstraint>);
 static_assert(ConstraintType<PoseEqualityConstraint>);
 static_assert(ConstraintType<TranslationEqualityConstraint>);
-static_assert(ConstraintType<PointLineRegionConstraint>);
 
-using Constraint =
-    std::variant<AngularVelocityMaxMagnitudeConstraint, LinePointConstraint,
-                 LinearAccelerationMaxMagnitudeConstraint,
-                 LinearVelocityDirectionConstraint,
-                 LinearVelocityMaxMagnitudeConstraint, PointAtConstraint,
-                 PointLineConstraint, PointPointMinConstraint,
-                 PointPointMaxConstraint, PoseEqualityConstraint,
-                 TranslationEqualityConstraint, PointLineRegionConstraint>;
+using Constraint = std::variant<
+    // clang-format off
+    AngularVelocityMaxMagnitudeConstraint,
+    LinePointConstraint,
+    LinearAccelerationMaxMagnitudeConstraint,
+    LinearVelocityDirectionConstraint,
+    LinearVelocityMaxMagnitudeConstraint,
+    PointAtConstraint,
+    PointLineConstraint,
+    PointLineRegionConstraint,
+    PointPointMaxConstraint,
+    PointPointMinConstraint,
+    PoseEqualityConstraint,
+    TranslationEqualityConstraint
+    // clang-format on
+    >;
 
 }  // namespace trajopt
