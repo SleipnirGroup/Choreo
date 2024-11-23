84a7355e096d95667c5dcad2cfba5da763b7037d
Jade Turner <spacey-sooty@proton.me>
2024-11-24 07:10:25 +0800

    [trajoptlib] Build and publish rust docs
    Resolves https://github.com/SleipnirGroup/Choreo/issues/942
    
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- make-docs.sh
+++ make-docs.sh
@@ -13,11 +13,14 @@ popd
 pushd trajoptlib
 mkdir -p build/docs
 doxygen docs/Doxyfile
+cargo doc
 popd
 
 mkdir -p site/api/{choreolib,trajoptlib}
 cp -r choreolib/build/docs/javadoc site/api/choreolib/java
 cp -r choreolib/build/docs/cpp/html site/api/choreolib/cpp
 cp -r trajoptlib/build/docs/html site/api/trajoptlib/cpp
+cp -r target/doc/trajoptlib site/api/trajoptlib/rs
 cp -r choreolib/vendor_jsons site/lib
+
 mkdocs build --dirty
