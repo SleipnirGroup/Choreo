938b475395fe4c2e57b050e15619a5405c5e3134
shueja-personal <j.a.shue@outlook.com>
2024-12-10 20:50:23 -0800

    LF line endings on test jsons

--- integration-tests/generate.sh
+++ integration-tests/generate.sh
@@ -1,4 +1,3 @@
-pnpm run build-cli
 mkdir test-tmp
 cp ./test-jsons/project/0/swerve.chor ./test-tmp/swerve.chor
 cp ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
@@ -6,4 +5,4 @@ cp ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
 ./target/${RUST_TOOLCHAIN}/release/choreo-cli --chor ./test-tmp/swerve.chor --all-trajectory --generate
 # Again to test if the cli produced loadable files
 ./target/${RUST_TOOLCHAIN}/release/choreo-cli --chor ./test-tmp/swerve.chor --all-trajectory --generate
-diff ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
+diff -u ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
