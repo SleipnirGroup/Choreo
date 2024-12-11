406548a6dbe5178e6b7239e136a442f5e64f6141
shueja-personal <j.a.shue@outlook.com>
2024-12-10 21:53:35 -0800

    Compare before and after second generate

--- integration-tests/generate.sh
+++ integration-tests/generate.sh
@@ -3,7 +3,11 @@ cp ./test-jsons/project/0/swerve.chor ./test-tmp/swerve.chor
 cp ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
 
 ./target/${RUST_TOOLCHAIN}/release/choreo-cli --chor ./test-tmp/swerve.chor --all-trajectory --generate
+cp ./test-tmp/swerve.traj ./test-tmp/swerve.traj.first
+
 # Again to test if the cli produced loadable files
 ./target/${RUST_TOOLCHAIN}/release/choreo-cli --chor ./test-tmp/swerve.chor --all-trajectory --generate
-diff -u ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
+# Same generation 
+diff -u ./test-tmp/swerve.traj.first ./test-tmp/swerve.traj
+# Verify no change to the .chor in the CLI
 diff -u ./test-jsons/project/0/swerve.chor ./test-tmp/swerve.chor
