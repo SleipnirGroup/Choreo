b50dc35dc7e21d4a9d02fb19154050439f235e71
shueja-personal <j.a.shue@outlook.com>
2024-12-10 20:13:21 -0800

    Fix chmod

--- integration-tests/generate.sh
+++ integration-tests/generate.sh
@@ -4,7 +4,7 @@ mkdir test-tmp
 cp ./test-jsons/project/0/swerve.chor ./test-tmp/swerve.chor
 cp ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
 
-./target/release/choreo-cli.exe --chor ./test-tmp/swerve.chor --all-trajectory --generate
+./target/release/Choreo.exe --chor ./test-tmp/swerve.chor --all-trajectory --generate
 # Again to test if the cli produced loadable files
-./target/release/choreo-cli.exe --chor ./test-tmp/swerve.chor --all-trajectory --generate
+./target/release/Choreo.exe --chor ./test-tmp/swerve.chor --all-trajectory --generate
 diff ./test-jsons/trajectory/0/swerve.traj ./test-tmp/swerve.traj
