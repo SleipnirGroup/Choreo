ae4c85b4093a96c5f0bc1038719c665181e3b69c
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-12-13 01:38:12 -0500

    removed nudging

--- src-core/src/spec/trajectory.rs
+++ src-core/src/spec/trajectory.rs
@@ -284,10 +284,7 @@ pub enum Sample {
         fr: f64,
     },
 }
-fn nudge_zero_and_round(mut input: f64) -> f64 {
-    if input.abs() < 1e-6 {
-        input = 0.0;
-    }
+fn round(input: f64) -> f64 {
     let factor = 100_000.0;
     (input * factor).round() / factor
 }
@@ -295,27 +292,27 @@ fn nudge_zero_and_round(mut input: f64) -> f64 {
 impl From<&SwerveTrajectorySample> for Sample {
     fn from(swerve_sample: &SwerveTrajectorySample) -> Self {
         Sample::Swerve {
-            t: nudge_zero_and_round(swerve_sample.timestamp),
-            x: nudge_zero_and_round(swerve_sample.x),
-            y: nudge_zero_and_round(swerve_sample.y),
-            vx: nudge_zero_and_round(swerve_sample.velocity_x),
-            vy: nudge_zero_and_round(swerve_sample.velocity_y),
-            heading: nudge_zero_and_round(swerve_sample.heading),
-            omega: nudge_zero_and_round(swerve_sample.angular_velocity),
-            ax: nudge_zero_and_round(swerve_sample.acceleration_x),
-            ay: nudge_zero_and_round(swerve_sample.acceleration_y),
-            alpha: nudge_zero_and_round(swerve_sample.angular_acceleration),
+            t: round(swerve_sample.timestamp),
+            x: round(swerve_sample.x),
+            y: round(swerve_sample.y),
+            vx: round(swerve_sample.velocity_x),
+            vy: round(swerve_sample.velocity_y),
+            heading: round(swerve_sample.heading),
+            omega: round(swerve_sample.angular_velocity),
+            ax: round(swerve_sample.acceleration_x),
+            ay: round(swerve_sample.acceleration_y),
+            alpha: round(swerve_sample.angular_acceleration),
             fx: [
-                nudge_zero_and_round(swerve_sample.module_forces_x[0]),
-                nudge_zero_and_round(swerve_sample.module_forces_x[1]),
-                nudge_zero_and_round(swerve_sample.module_forces_x[2]),
-                nudge_zero_and_round(swerve_sample.module_forces_x[3]),
+                round(swerve_sample.module_forces_x[0]),
+                round(swerve_sample.module_forces_x[1]),
+                round(swerve_sample.module_forces_x[2]),
+                round(swerve_sample.module_forces_x[3]),
             ],
             fy: [
-                nudge_zero_and_round(swerve_sample.module_forces_y[0]),
-                nudge_zero_and_round(swerve_sample.module_forces_y[1]),
-                nudge_zero_and_round(swerve_sample.module_forces_y[2]),
-                nudge_zero_and_round(swerve_sample.module_forces_y[3]),
+                round(swerve_sample.module_forces_y[0]),
+                round(swerve_sample.module_forces_y[1]),
+                round(swerve_sample.module_forces_y[2]),
+                round(swerve_sample.module_forces_y[3]),
             ],
         }
     }
@@ -329,16 +326,16 @@ impl From<SwerveTrajectorySample> for Sample {
 impl From<&DifferentialTrajectorySample> for Sample {
     fn from(differential_sample: &DifferentialTrajectorySample) -> Self {
         Sample::DifferentialDrive {
-            t: nudge_zero_and_round(differential_sample.timestamp),
-            x: nudge_zero_and_round(differential_sample.x),
-            y: nudge_zero_and_round(differential_sample.y),
-            heading: nudge_zero_and_round(differential_sample.heading),
-            vl: nudge_zero_and_round(differential_sample.velocity_l),
-            vr: nudge_zero_and_round(differential_sample.velocity_r),
-            al: nudge_zero_and_round(differential_sample.acceleration_l),
-            ar: nudge_zero_and_round(differential_sample.acceleration_r),
-            fl: nudge_zero_and_round(differential_sample.force_l),
-            fr: nudge_zero_and_round(differential_sample.force_r),
+            t: round(differential_sample.timestamp),
+            x: round(differential_sample.x),
+            y: round(differential_sample.y),
+            heading: round(differential_sample.heading),
+            vl: round(differential_sample.velocity_l),
+            vr: round(differential_sample.velocity_r),
+            al: round(differential_sample.acceleration_l),
+            ar: round(differential_sample.acceleration_r),
+            fl: round(differential_sample.force_l),
+            fr: round(differential_sample.force_r),
         }
     }
 }
