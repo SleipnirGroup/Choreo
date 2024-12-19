9fcb5da77e494f38763c9297c40ff4b0c0c775b7
shueja-personal <j.a.shue@outlook.com>
2024-12-19 14:30:36 -0800

    Add CI to publish an artifact for choreolib integration tests

--- choreolib/src/test/java/choreo/trajectory/TrajectoryTestHelper.java
+++ choreolib/src/test/java/choreo/trajectory/TrajectoryTestHelper.java
@@ -3,7 +3,13 @@
 package choreo.trajectory;
 
 import edu.wpi.first.math.geometry.Pose2d;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrajectoryTestHelper {
   private static final double DT = 0.005;
@@ -58,4 +64,22 @@ public class TrajectoryTestHelper {
 
     return (Trajectory<SampleType>) trajectory;
   }
+
+  /**
+ * Reads given resource file as a string.
+ *
+ * @param fileName path to the resource file
+ * @return the file's contents
+ * @throws IOException if read fails for any reason
+ */
+public static String getResourceFileAsString(String fileName) throws IOException {
+    ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+        if (is == null) return null;
+        try (InputStreamReader isr = new InputStreamReader(is);
+             BufferedReader reader = new BufferedReader(isr)) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+}
 }
