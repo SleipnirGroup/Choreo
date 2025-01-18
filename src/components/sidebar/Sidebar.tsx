288764094064e4a41d517fad169266310443bae9
shueja-personal <j.a.shue@outlook.com>
2025-01-17 22:53:31 -0800

    [ui] Add a "Generate All Outdated" button

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { Divider, IconButton, Tooltip } from "@mui/material";
 import WaypointList from "./WaypointList";
 import PathSelector from "./PathSelector";
 import MenuIcon from "@mui/icons-material/Menu";
-import { ContentCopy, Redo, Undo } from "@mui/icons-material";
+import { ContentCopy, Redo, ShapeLine, Undo } from "@mui/icons-material";
 import Add from "@mui/icons-material/Add";
 import SidebarConstraint from "./SidebarConstraint";
 import SidebarEventMarker from "./SidebarEventMarker";
@@ -80,9 +80,26 @@ class Sidebar extends Component<Props, State> {
         </div>
         <div
           className={styles.SidebarHeading}
-          style={{ gridTemplateColumns: "auto 33.6px 33.6px" }}
+          style={{ gridTemplateColumns: "auto 33.6px 33.6px 33.6px" }}
         >
           PATHS
+          <Tooltip disableInteractive title="Generate All Outdated">
+            <span>
+              <IconButton
+                size="small"
+                color="default"
+                style={{
+                  float: "right"
+                }}
+                disabled={Object.keys(doc.pathlist.paths).length == 0}
+                onClick={() =>
+                  doc.generateAllOutdated()
+                }
+              >
+                <ShapeLine fontSize="small"></ShapeLine>
+              </IconButton>
+            </span>
+          </Tooltip>
           <Tooltip disableInteractive title="Duplicate Path">
             <span>
               <IconButton
