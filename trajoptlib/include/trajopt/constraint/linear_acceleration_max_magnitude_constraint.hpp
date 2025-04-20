3093b5b5743a433188b4679cc858b5127ab8b526
Tyler Veness <calcmogul@gmail.com>
2025-04-19 19:05:29 -0700

    [trajoptlib] Rename files to snake case (#1229)

---
+++ trajoptlib/include/trajopt/constraint/linear_acceleration_max_magnitude_constraint.hpp
@@ -0,0 +1,60 @@
+// Copyright (c) TrajoptLib contributors
+
+#pragma once
+
+#include <cassert>
+
+#include <sleipnir/autodiff/variable.hpp>
+#include <sleipnir/optimization/problem.hpp>
+
+#include "trajopt/geometry/pose2.hpp"
+#include "trajopt/geometry/translation2.hpp"
+#include "trajopt/util/symbol_exports.hpp"
+
+namespace trajopt {
+
+/**
+ * Linear acceleration max magnitude inequality constraint.
+ */
+class TRAJOPT_DLLEXPORT LinearAccelerationMaxMagnitudeConstraint {
+ public:
+  /**
+   * Constructs a LinearAccelerationMaxMagnitudeConstraint.
+   *
+   * @param maxMagnitude The maximum linear acceleration magnitude. Must be
+   *     nonnegative.
+   */
+  explicit LinearAccelerationMaxMagnitudeConstraint(double maxMagnitude)
+      : m_maxMagnitude{maxMagnitude} {
+    assert(maxMagnitude >= 0.0);
+  }
+
+  /**
+   * Applies this constraint to the given problem.
+   *
+   * @param problem The optimization problem.
+   * @param pose The robot's pose.
+   * @param linearVelocity The robot's linear velocity.
+   * @param angularVelocity The robot's angular velocity.
+   * @param linearAcceleration The robot's linear acceleration.
+   * @param angularAcceleration The robot's angular acceleration.
+   */
+  void Apply(slp::Problem& problem, [[maybe_unused]] const Pose2v& pose,
+             [[maybe_unused]] const Translation2v& linearVelocity,
+             [[maybe_unused]] const slp::Variable& angularVelocity,
+             const Translation2v& linearAcceleration,
+             [[maybe_unused]] const slp::Variable& angularAcceleration) {
+    if (m_maxMagnitude == 0.0) {
+      problem.subject_to(linearAcceleration.X() == 0.0);
+      problem.subject_to(linearAcceleration.Y() == 0.0);
+    } else {
+      problem.subject_to(linearAcceleration.SquaredNorm() <=
+                         m_maxMagnitude * m_maxMagnitude);
+    }
+  }
+
+ private:
+  double m_maxMagnitude;
+};
+
+}  // namespace trajopt
