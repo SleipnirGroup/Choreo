80e27ac508cb2570ee17269c63bb9116bc1a7647
Tyler Veness <calcmogul@gmail.com>
2025-10-14 14:48:52 -0700

    Simplify CMake settings (#1319)

--- trajoptlib/cmake/CompilerFlags.cmake
+++ trajoptlib/cmake/CompilerFlags.cmake
@@ -1,26 +1,13 @@
 macro(compiler_flags target)
     if(NOT MSVC)
-        target_compile_options(
-            ${target}
-            PRIVATE -Wall -pedantic -Wextra -Werror
-        )
+        target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic)
     else()
         # Suppress the following warnings:
         #   * C4244: lossy conversion
         #   * C4251: missing dllexport/dllimport attribute on data member
-        target_compile_options(${target} PRIVATE /wd4244 /wd4251 /WX)
+        target_compile_options(${target} PRIVATE /wd4244 /wd4251)
     endif()
-
-    # Disable warning false positives in Eigen
-    if(
-        ${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU"
-        AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL "12"
-    )
-        target_compile_options(
-            ${target}
-            PRIVATE -Wno-array-bounds -Wno-stringop-overflow
-        )
-    endif()
+    set_property(TARGET ${target} PROPERTY COMPILE_WARNING_AS_ERROR ON)
 
     target_compile_features(${target} PUBLIC cxx_std_23)
     if(MSVC)
