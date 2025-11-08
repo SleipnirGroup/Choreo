50b77a80e1af4731b90c174ede9ff82e15c74ce2
Tinbite-A <tinbite724@gmail.com>
2025-11-08 00:27:59 -0500

    added bunnybots 2025 MD field

--- choreolib/src/main/native/include/choreo/util/FieldDimensions.h
+++ choreolib/src/main/native/include/choreo/util/FieldDimensions.h
@@ -5,6 +5,6 @@
 #include <units/length.h>
 
 namespace choreo::util {
-static constexpr units::meter_t fieldLength = 17.548_m;
-static constexpr units::meter_t fieldWidth = 8.052_m;
+static constexpr units::meter_t fieldLength = 16.637_m;
+static constexpr units::meter_t fieldWidth = 8.407_m;
 }  // namespace choreo::util
