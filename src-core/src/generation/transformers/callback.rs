88c29a0d17d1b2f79019235cba643e24e638a92d
Tyler Veness <calcmogul@gmail.com>
2025-11-25 13:06:32 -0800

    Run rustfmt on all files

--- src-core/src/generation/transformers/callback.rs
+++ src-core/src/generation/transformers/callback.rs
@@ -1,25 +1,32 @@
 use trajoptlib::{DifferentialTrajectory, SwerveTrajectory};
 
-use crate::{generation::generate::{LocalProgressUpdate, PROGRESS_SENDER_LOCK}, ResultExt};
+use crate::{
+    generation::generate::{LocalProgressUpdate, PROGRESS_SENDER_LOCK},
+    ResultExt,
+};
 
-use super::{DifferentialGenerationTransformer, FeatureLockedTransformer, GenerationContext, SwerveGenerationTransformer};
-
+use super::{
+    DifferentialGenerationTransformer, FeatureLockedTransformer, GenerationContext,
+    SwerveGenerationTransformer,
+};
 
 pub struct CallbackSetter;
 
 fn swerve_status_callback(trajectory: SwerveTrajectory, handle: i64) {
     let tx_opt = PROGRESS_SENDER_LOCK.get();
     if let Some(tx) = tx_opt {
-        let _  = tx.send(LocalProgressUpdate::from(trajectory).handled(handle))
-        .trace_warn();
+        let _ = tx
+            .send(LocalProgressUpdate::from(trajectory).handled(handle))
+            .trace_warn();
     };
 }
 
 fn differential_status_callback(trajectory: DifferentialTrajectory, handle: i64) {
     let tx_opt = PROGRESS_SENDER_LOCK.get();
     if let Some(tx) = tx_opt {
-        let _ = tx.send(LocalProgressUpdate::from(trajectory).handled(handle))
-        .trace_warn();
+        let _ = tx
+            .send(LocalProgressUpdate::from(trajectory).handled(handle))
+            .trace_warn();
     };
 }
 
