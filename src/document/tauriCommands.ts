c6c2bc618fa4b3ae9a12b9cf6a7bacf85bdbcaf3
shueja-personal <j.a.shue@outlook.com>
2026-01-03 23:52:14 -0800

    Limit number of parallel generation jobs

--- src/document/tauriCommands.ts
+++ src/document/tauriCommands.ts
@@ -6,7 +6,7 @@ export type ChoreoResult<T> = T | ChoreoError;
 export const Commands = {
   guessIntervals: (config: RobotConfig<Expr>, trajectory: Trajectory) =>
     invoke<number[]>("guess_control_interval_counts", { config, trajectory }),
-
+  getWorkerCount: () => invoke<number>("get_worker_count"),
   /**
    * Generates a `Trajectory` using the specified `Project` and `Trajectory`.
    *
