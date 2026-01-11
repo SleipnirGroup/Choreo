bacfc23c61cf11e78b5ffefc61df33bde0b28219
shueja-personal <j.a.shue@outlook.com>
2026-01-11 13:14:54 -0800

    Refactor and format

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from 'vite-plugin-svgr';
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
