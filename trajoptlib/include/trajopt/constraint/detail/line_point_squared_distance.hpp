6fd73463d6dae34943aac4b440b6c1cf883fc457
Tyler Veness <calcmogul@gmail.com>
2026-01-30 16:37:54 -0800

    [trajoptlib] Upgrade to Sleipnir 0.4.0 to remove max() and min() shims

--- trajoptlib/include/trajopt/constraint/detail/line_point_squared_distance.hpp
+++ trajoptlib/include/trajopt/constraint/detail/line_point_squared_distance.hpp
@@ -15,12 +15,6 @@ decltype(auto) line_point_squared_distance(const Translation2<T>& line_start,
                                            const Translation2<U>& point) {
   using R = decltype(std::declval<T>() + std::declval<U>());
 
-  auto max = [](R a, R b) {
-    return +0.5 * (1 + slp::sign(b - a)) * (b - a) + a;
-  };
-  auto min = [](R a, R b) {
-    return -0.5 * (1 + slp::sign(b - a)) * (b - a) + b;
-  };
   auto lerp = [](R a, R b, R t) { return a + t * (b - a); };
 
   Translation2<R> l{line_end.x() - line_start.x(),
@@ -28,7 +22,7 @@ decltype(auto) line_point_squared_distance(const Translation2<T>& line_start,
   Translation2<R> v{point.x() - line_start.x(), point.y() - line_start.y()};
 
   auto t = v.dot(l) / l.squared_norm();
-  auto t_bounded = max(min(t, 1), 0);  // NOLINT(build/include_what_you_use)
+  auto t_bounded = slp::max(slp::min(t, 1), 0);
 
   Translation2<R> i{lerp(line_start.x(), line_end.x(), t_bounded),
                     lerp(line_start.y(), line_end.y(), t_bounded)};
