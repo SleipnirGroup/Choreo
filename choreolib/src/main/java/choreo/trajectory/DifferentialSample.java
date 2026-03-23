23825fb675ff740a474161a57bb7578360ab934e
Rohan Bhattacharyya <dev.creator42@gmail.com>
2026-03-23 16:32:55 -0700

    Revert heading comparison to raw isNear

--- choreolib/src/main/java/choreo/trajectory/DifferentialSample.java
+++ choreolib/src/main/java/choreo/trajectory/DifferentialSample.java
@@ -271,7 +271,7 @@ public class DifferentialSample implements TrajectorySample<DifferentialSample> 
     return MathUtil.isNear(this.t, other.t, 1E-6)
         && MathUtil.isNear(this.x, other.x, 1E-6)
         && MathUtil.isNear(this.y, other.y, 1E-6)
-        && MathUtil.isNear(MathUtil.angleModulus(this.heading - other.heading), 0.0, 1E-6)
+        && MathUtil.isNear(this.heading, other.heading, 1E-6)
         && MathUtil.isNear(this.vl, other.vl, 1E-6)
         && MathUtil.isNear(this.vr, other.vr, 1E-6)
         && MathUtil.isNear(this.omega, other.omega, 1E-6)
