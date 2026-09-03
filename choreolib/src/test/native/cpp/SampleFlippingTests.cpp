2fc328394bbcf679a7ea69f791e98249f6324f18
Tyler Veness <calcmogul@gmail.com>
2026-09-02 18:02:38 -0700

    Migrate to Catch2

--- choreolib/src/test/native/cpp/SampleFlippingTests.cpp
+++ choreolib/src/test/native/cpp/SampleFlippingTests.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
 #include <wpi/units/force.hpp>
 #include <wpi/util/json.hpp>
 
@@ -12,7 +12,7 @@
 
 using namespace choreo;
 
-TEST(SampleFlippingTest, ZeroSwerveSample) {
+TEST_CASE("SampleFlippingTest ZeroSwerveSample", "[choreolib]") {
   try {
     SwerveSample sample{0_s,
                         0_m,
@@ -51,15 +51,14 @@ TEST(SampleFlippingTest, ZeroSwerveSample) {
                              {0_N, 0_N, 0_N, 0_N},
                              {0_N, 0_N, 0_N, 0_N}};
 
-    ASSERT_TRUE(sample.Flipped<2024>() == mirrored2024);
-    ASSERT_TRUE(sample.Flipped<2022>() == rotated2022);
+    REQUIRE(sample.Flipped<2024>() == mirrored2024);
+    REQUIRE(sample.Flipped<2022>() == rotated2022);
   } catch (std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    FAIL();
+    FAIL(e.what());
   }
 }
 
-TEST(SampleFlippingTest, SwerveSample) {
+TEST_CASE("SampleFlippingTest SwerveSample", "[choreolib]") {
   try {
     SwerveSample sample{0_s,
                         1_m,
@@ -98,15 +97,14 @@ TEST(SampleFlippingTest, SwerveSample) {
                              {-10_N, -11_N, -12_N, -13_N},
                              {-14_N, -15_N, -16_N, -17_N}};
 
-    ASSERT_TRUE(sample.Flipped<2024>() == mirrored2024);
-    ASSERT_TRUE(sample.Flipped<2022>() == rotated2022);
+    REQUIRE(sample.Flipped<2024>() == mirrored2024);
+    REQUIRE(sample.Flipped<2022>() == rotated2022);
   } catch (std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    FAIL();
+    FAIL(e.what());
   }
 }
 
-TEST(SampleFlippingTest, ZeroDifferentialSample) {
+TEST_CASE("SampleFlippingTest ZeroDifferentialSample", "[choreolib]") {
   try {
     DifferentialSample sample{
         0_s,         0_m,      0_m,      0_rad,          0_mps, 0_mps,
@@ -131,15 +129,14 @@ TEST(SampleFlippingTest, ZeroDifferentialSample) {
                                    0_N,
                                    0_N};
 
-    ASSERT_TRUE(sample.Flipped<2024>() == mirrored2024);
-    ASSERT_TRUE(sample.Flipped<2022>() == rotated2022);
+    REQUIRE(sample.Flipped<2024>() == mirrored2024);
+    REQUIRE(sample.Flipped<2022>() == rotated2022);
   } catch (std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    FAIL();
+    FAIL(e.what());
   }
 }
 
-TEST(SampleFlippingTest, DifferentialSample) {
+TEST_CASE("SampleFlippingTest DifferentialSample", "[choreolib]") {
   try {
     DifferentialSample sample{
         0_s,         1_m,      2_m,      3_rad,          4_mps, 5_mps,
@@ -164,10 +161,9 @@ TEST(SampleFlippingTest, DifferentialSample) {
                                    10_N,
                                    11_N};
 
-    ASSERT_TRUE(sample.Flipped<2024>() == mirrored2024);
-    ASSERT_TRUE(sample.Flipped<2022>() == rotated2022);
+    REQUIRE(sample.Flipped<2024>() == mirrored2024);
+    REQUIRE(sample.Flipped<2022>() == rotated2022);
   } catch (std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    FAIL();
+    FAIL(e.what());
   }
 }
--- choreolib/src/test/native/cpp/TrajectoryFileTests.cpp
+++ choreolib/src/test/native/cpp/TrajectoryFileTests.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
 #include <wpi/units/force.hpp>
 #include <wpi/util/json.hpp>
 
@@ -107,14 +107,12 @@ const Trajectory<SwerveSample> correctSwerveTrajectory{
     {0},
     {{0_s, "testEvent"}}};
 
-TEST(TrajectoryFileTest, DeserializeSwerveTrajectory) {
+TEST_CASE("TrajectoryFileTest DeserializeSwerveTrajectory", "[choreolib]") {
   try {
     Trajectory<SwerveSample> deserializedSwerveTrajectory =
         swerveTrajectoryJson.get<Trajectory<SwerveSample>>();
-    ASSERT_EQ(correctSwerveTrajectory, deserializedSwerveTrajectory);
+    REQUIRE(correctSwerveTrajectory == deserializedSwerveTrajectory);
   } catch (std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    FAIL();
+    FAIL(e.what());
   }
-  SUCCEED();
 }
