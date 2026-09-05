78133653e485bec2b24b3ffae19f08c4a2fa2133
Jade Turner <spacey-sooty@proton.me>
2026-09-05 14:32:05 +0800

    add rust docs to site

--- make-docs.sh
+++ make-docs.sh
@@ -20,7 +20,7 @@ mkdir -p site/api/{choreolib,trajoptlib}
 cp -r choreolib/build/docs/javadoc site/api/choreolib/java
 cp -r choreolib/build/docs/cpp/html site/api/choreolib/cpp
 cp -r trajoptlib/build/docs/html site/api/trajoptlib/cpp
-cp -r target/doc/trajoptlib site/api/trajoptlib/rs
+cp -r target/doc site/api/trajoptlib/rs
 cp -r choreolib/vendor_jsons site/lib
 
 mkdocs build --dirty
